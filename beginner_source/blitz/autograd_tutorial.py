146c2b382876cb3b67feff4b10708fd47b6fdbb3
Junghwan Park <reserve.dev@gmail.com>
2018-05-09 00:18:50 +0900

    초벌 번역 완료

--- beginner_source/blitz/autograd_tutorial.py
+++ beginner_source/blitz/autograd_tutorial.py
@@ -1,80 +1,76 @@
 # -*- coding: utf-8 -*-
 """
-Autograd: automatic differentiation
+Autograd: 자동 미분
 ===================================
 
-Central to all neural networks in PyTorch is the ``autograd`` package.
-Let’s first briefly visit this, and we will then go to training our
-first neural network.
+PyTorch의 모든 신경망의 중심에는 ``autograd`` 패키지가 있습니다.
+먼저 이것을 가볍게 살펴본 뒤, 첫번째 신경망을 학습시켜보겠습니다.
 
 
-The ``autograd`` package provides automatic differentiation for all operations
-on Tensors. It is a define-by-run framework, which means that your backprop is
-defined by how your code is run, and that every single iteration can be
-different.
+``autograd`` 패키지는 Tensor의 모든 연산에 대해 자동 미분을 제공합니다.
+이는 실행-기반-정의(define-by-run) 프레임워크로, 이는 코드를 어떻게 작성하여 실행하느냐에
+따라 역전파가 정의된다는 뜻이며, 역전파는 학습 과정의 매 단계마다 달라집니다.
 
-Let us see this in more simple terms with some examples.
+좀 더 간단한 용어로 몇 가지 예를 보이겠습니다.
 
 Tensor
 --------
 
-``torch.Tensor`` is the central class of the package. If you set its attribute
-``.requires_grad`` as ``True``, it starts to track all operations on it. When
-you finish your computation you can call ``.backward()`` and have all the
-gradients computed automatically. The gradient for this tensor will be
-accumulated into ``.grad`` attribute.
+패키지의 중심에는 ``torch.Tensor`` 클래스가 있습니다. 만약 ``.requires_grad``
+속성을 ``True`` 로 설정하면, 그 tensor에서 이뤄진 모든 연산들을 추적(Track)하기
+시작합니다. 계산이 완료된 후 ``.backward()`` 를 호출하여 모든 변화도(gradient)를
+자동으로 계산할 수 있습니다. 이 Tensor의 변화도는 ``.grad`` 에 누적됩니다.
 
-To stop a tensor from tracking history, you can call ``.detach()`` to detach
-it from the computation history, and to prevent future computation from being
-tracked.
+Tensor가 기록을 중단하게 하려면, ``.detach()`` 를 호출하여 연산 기록으로부터
+분리(detach)하여 이후 연산들이 기록되는 것을 방지할 수 있습니다.
 
-To prevent tracking history (and using memory), you can also wrap the code block
-in ``with torch.no_grad():``. This can be particularly helpful when evaluating a
-model because the model may have trainable parameters with `requires_grad=True`,
-but we don't need the gradients.
+연산 기록을 추적하는 것(과 메모리 사용)을 멈추기 위해, 코드 블럭(Code Block)을
+``with torch.no_grad():`` 로 감쌀 수 있습니다. 이는 특히 변화도(Gradient)는
+필요없지만, `requires_grad=True` 가 설정되어 학습 가능한 매개변수(Parameter)를
+갖는 모델을 평가(Evaluate)할 때 유용합니다.
 
-There’s one more class which is very important for autograd
-implementation - a ``Function``.
+Autograd 구현에서 매우 중요한 클래스가 하나 더 있는데요, 바로 ``Function``
+클래스입니다.
 
-``Tensor`` and ``Function`` are interconnected and build up an acyclic
-graph, that encodes a complete history of computation. Each variable has
-a ``.grad_fn`` attribute that references a ``Function`` that has created
-the ``Tensor`` (except for Tensors created by the user - their
-``grad_fn is None``).
+``Tensor`` 와 ``Function`` 은 상호 연결되어 있으며, 모든 연산 과정을
+부호화(encode)하여 순환하지 않은 그래프(acyclic graph)를 생성합니다. 각 변수는
+``.grad_fn`` 속성을 갖고 있는데, 이는 ``Tensor`` 를 생성한 ``Function`` 을
+참조하고 있습니다. (단, 사용자가 만든 Tensor는 예외로, 이 때 ``grad_fn`` 은
+``None`` 입니다.)
 
-If you want to compute the derivatives, you can call ``.backward()`` on
-a ``Tensor``. If ``Tensor`` is a scalar (i.e. it holds a one element
-data), you don’t need to specify any arguments to ``backward()``,
-however if it has more elements, you need to specify a ``gradient``
-argument that is a tensor of matching shape.
+도함수를 계산하기 위해서는, ``Tensor`` 의 ``.backward()`` 를 호출하면 됩니다.
+``Tensor`` 가 스칼라(scalar)인 경우(예. 하나의 요소만 갖는 등)에는, ``backward`` 에
+인자를 정해줄 필요가 없습니다. 하지만 여러 개의 요소를 갖고 있을
+때는 tensor의 모양을 ``gradient`` 의 인자로 지정할 필요가 있습니다.
+
 """
 
 import torch
 
 ###############################################################
-# Create a tensor and set requires_grad=True to track computation with it
+# tensor를 생성하고 requires_grad=True를 설정하여 연산을 기록합니다.
 x = torch.ones(2, 2, requires_grad=True)
 print(x)
 
 ###############################################################
-# Do an operation of tensor:
+# tensor에 연산을 수행합니다:
 y = x + 2
 print(y)
 
 ###############################################################
-# ``y`` was created as a result of an operation, so it has a ``grad_fn``.
+# ``y`` 는 연산의 결과로 생성된 것이므로, ``grad_fn`` 을 갖습니다.
 print(y.grad_fn)
 
 ###############################################################
-# Do more operations on y
+# y에 다른 연산을 수행합니다.
 z = y * y * 3
 out = z.mean()
 
 print(z, out)
 
 ################################################################
-# ``.requires_grad_( ... )`` changes an existing Tensor's ``requires_grad``
-# flag in-place. The input flag defaults to ``True`` if not given.
+# ``.requires_grad_( ... )`` 은 기존 Tensor의 ``requires_grad`` 값을 In-place로
+# 변화시킵니다. 입력 값이 주어지지 않으면 기본 값으로 ``True`` 를 갖습니다.
 a = torch.randn(2, 2)
 a = ((a * 3) / (a - 1))
 print(a.requires_grad)
@@ -84,31 +80,31 @@ b = (a * a).sum()
 print(b.grad_fn)
 
 ###############################################################
-# Gradients
-# ---------
-# Let's backprop now
-# Because ``out`` contains a single scalar, ``out.backward()`` is
-# equivalent to ``out.backward(torch.tensor(1))``.
+# 변화도(Gradient)
+# -----------------
+# 이제 역전파(backprop)를 해보겠습니다.
+# ``out`` 은 하나의 스칼라(Scalar) 값만 갖고 있기 때문에, ``out.backward()`` 는
+# ``out.backward(torch.tensor(1))`` 을 하는 것과 똑같습니다.
 
 out.backward()
 
 ###############################################################
-# print gradients d(out)/dx
+# 변화도 d(out)/dx를 출력합니다.
 #
 
 print(x.grad)
 
 ###############################################################
-# You should have got a matrix of ``4.5``. Let’s call the ``out``
-# *Tensor* “:math:`o`”.
-# We have that :math:`o = \frac{1}{4}\sum_i z_i`,
-# :math:`z_i = 3(x_i+2)^2` and :math:`z_i\bigr\rvert_{x_i=1} = 27`.
-# Therefore,
-# :math:`\frac{\partial o}{\partial x_i} = \frac{3}{2}(x_i+2)`, hence
+# ``4.5`` 로 이루어진 행렬이 보일 것입니다. ``out`` 을 *Tensor* “:math:`o`” 라고
+# 하면, 다음과 같이 구할 수 있습니다.
+# :math:`o = \frac{1}{4}\sum_i z_i`,
+# :math:`z_i = 3(x_i+2)^2` 이고 :math:`z_i\bigr\rvert_{x_i=1} = 27` 입니다.
+# 따라서,
+# :math:`\frac{\partial o}{\partial x_i} = \frac{3}{2}(x_i+2)` 이므로,
 # :math:`\frac{\partial o}{\partial x_i}\bigr\rvert_{x_i=1} = \frac{9}{2} = 4.5`.
 
 ###############################################################
-# You can do many crazy things with autograd!
+# autograd로 많은 정신나간 일들(crazy things)도 할 수 있습니다!
 
 
 x = torch.randn(3, requires_grad=True)
@@ -127,9 +123,8 @@ y.backward(gradients)
 print(x.grad)
 
 ###############################################################
-# You can also stops autograd from tracking history on Tensors
-# with requires_grad=True by wrapping the code block in
-# ``with torch.no_grad():``
+# ``with torch.no_grad():`` 로 코드 블럭(Code Block)을 감싸서, autograd가
+# requires_grad=True인 Tensor들의 연산 기록을 추적하는 것을 멈출 수 있습니다.
 print(x.requires_grad)
 print((x ** 2).requires_grad)
 
@@ -137,7 +132,6 @@ with torch.no_grad():
 	print((x ** 2).requires_grad)
 
 ###############################################################
-# **Read Later:**
+# **더 읽을거리:**
 #
-# Documentation of ``autograd`` and ``Function`` is at
-# http://pytorch.org/docs/autograd
+# ``Variable`` 과 ``Function`` 관련 문서는 http://pytorch.org/docs/autograd 에 있습니다.
