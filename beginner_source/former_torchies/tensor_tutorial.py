cc0df12c9911bd62476d8e69b05e013477198f7c
Junghwan Park <reserve.dev@gmail.com>
2018-05-12 11:08:37 +0900

    초벌 번역 완료

--- beginner_source/former_torchies/tensor_tutorial.py
+++ beginner_source/former_torchies/tensor_tutorial.py
@@ -1,11 +1,10 @@
 """
-Tensors
+Tensor
 =======
 
-Tensors behave almost exactly the same way in PyTorch as they do in
-Torch.
+PyTorch에서의 Tensor는 Torch에서와 거의 동일하게 동작합니다.
 
-Create a tensor of size (5 x 7) with uninitialized memory:
+초기화되지 않은 (5 x 7) 크기의 tensor를 생성합니다:
 
 """
 
@@ -13,8 +12,7 @@ import torch
 a = torch.empty(5, 7, dtype=torch.float)
 
 ###############################################################
-# Initialize a double tensor randomized with a normal distribution with mean=0,
-# var=1:
+# 평균 0, 분산 1의 정규분포를 따르는 무작위 숫자로 dobule tensor를 초기화합니다:
 
 a = torch.randn(5, 7, dtype=torch.double)
 print(a)
@@ -22,49 +20,48 @@ print(a.size())
 
 ###############################################################
 # .. note::
-#     ``torch.Size`` is in fact a tuple, so it supports the same operations
+#     ``torch.Size`` 는 튜플(tuple)과 같으며, 모든 튜플 연산에 사용할 수 있습니다.
 #
-# Inplace / Out-of-place
-# ----------------------
+# In-place / Out-of-place
+# ------------------------
 #
-# The first difference is that ALL operations on the tensor that operate
-# in-place on it will have an ``_`` postfix. For example, ``add`` is the
-# out-of-place version, and ``add_`` is the in-place version.
+# 첫 번째 차이점은 tensor의 모든 In-place 연산은 ``_`` 접미사를 갖는다는 것입니다.
+# 예를 들어, ``add`` 는 연산 결과를 돌려주는 Out-of-place 연산을 하고, ``add_`` 는
+# In-place 연산을 합니다.
 
 a.fill_(3.5)
-# a has now been filled with the value 3.5
+# a가 값 3.5로 채워집니다.
 
 b = a.add(4.0)
-# a is still filled with 3.5
-# new tensor b is returned with values 3.5 + 4.0 = 7.5
+# a는 여전히 3.5입니다.
+# 3.5 + 4.0 = 7.5의 값이 반환되어 새로운 tensor b가 됩니다.
 
 print(a, b)
 
 ###############################################################
-# Some operations like ``narrow`` do not have in-place versions, and
-# hence, ``.narrow_`` does not exist. Similarly, some operations like
-# ``fill_`` do not have an out-of-place version, so ``.fill`` does not
-# exist.
+# ``narrow`` 와 같은 일부 연산들은 In-place 형태를 갖지 않기 때문에 ``.narrow_`` 는
+# 존재하지 않습니다. 또한, ``fill_`` 은 Out-of-place 형태를 갖지 않기 떄문에 역시
+# ``.fill`` 도 존재하지 않습니다.
 #
-# Zero Indexing
-# -------------
+# 0-인덱스(Zero Indexing)
+# -----------------------
 #
-# Another difference is that Tensors are zero-indexed. (In lua, tensors are
-# one-indexed)
+# 또 다른 차이점은 Tensor의 인덱스는 0부터 시작(0-인덱스)는 점입니다.
+# (Lua에서 tensor는 1-인덱스를 갖습니다.)
 
 b = a[0, 3]  # select 1st row, 4th column from a
 
 ###############################################################
-# Tensors can be also indexed with Python's slicing
+# Python의 슬라이싱(slicing)으로도 Tensor를 인덱스 할 수 있습니다.
 
 b = a[:, 3:5]  # selects all rows, 4th column and  5th column from a
 
 ###############################################################
-# No camel casing
-# ---------------
+# 카멜표기법(Camel Case) 없음
+# ----------------------------
 #
-# The next small difference is that all functions are now NOT camelCase
-# anymore. For example ``indexAdd`` is now called ``index_add_``
+# 그 외에도 카멜표기법을 사용하지 않는 사소한 차이가 있습니다.
+# 예를 들어 ``indexAdd`` 는 ``index_add_`` 라고 표기합니다.
 
 
 x = torch.ones(5, 5)
@@ -84,14 +81,14 @@ x.index_add_(1, torch.tensor([4, 0], dtype=torch.long), z)
 print(x)
 
 ###############################################################
-# Numpy Bridge
-# ------------
+# NumPy 변환(Bridge)
+# ------------------
 #
-# Converting a torch Tensor to a numpy array and vice versa is a breeze.
-# The torch Tensor and numpy array will share their underlying memory
-# locations, and changing one will change the other.
+# Torch Tensor를 NumPy 배열(array)로 변환하거나, 그 반대로 하는 것은 매우 쉽습니다.
+# Torch Tensor와 NumPy 배열은 저장 공간을 공유하기 때문에, 하나를 변경하면 다른 하나도
+# 변경됩니다.
 #
-# Converting torch Tensor to numpy Array
+# Torch Tensor를 NumPy 배열로 변환하기
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 a = torch.ones(5)
@@ -111,7 +108,7 @@ print(b) 	# see how the numpy array changed in value
 
 
 ###############################################################
-# Converting numpy Array to torch Tensor
+# NumPy 배열을 Torch Tensor로 변환하기
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 import numpy as np
@@ -122,22 +119,20 @@ print(a)
 print(b)  # see how changing the np array changed the torch Tensor automatically
 
 ###############################################################
-# All the Tensors on the CPU except a CharTensor support converting to
-# NumPy and back.
+# CharTensor를 제외한 CPU 상의 모든 Tensor는 NumPy로의 변환을 지원하며,
+# (NumPy에서 Tensor로의) 반대 변환도 지원합니다.
 #
 # CUDA Tensors
 # ------------
 #
-# CUDA Tensors are nice and easy in pytorch, and transfering a CUDA tensor
-# from the CPU to GPU will retain its underlying type.
+# PyTorch에서 CUDA Tensor는 멋지고 쉽습니다. 그리고 CUDA Tensor를 CPU에서 GPU로 옮겨도
+# 기본 형식(underlying type)은 유지됩니다.
 
-# let us run this cell only if CUDA is available
+# 이 코드는 CUDA가 사용 가능한 환경에서만 실행됩니다.
 if torch.cuda.is_available():
 
-    # creates a LongTensor and transfers it
-    # to GPU as torch.cuda.LongTensor
+    # LongTensor를 생성하고 이를 torch.cuda.LongTensor로 GPU로 옮깁니다.
     a = torch.full((10,), 3, device=torch.device("cuda"))
     print(type(a))
     b = a.to(torch.device("cpu"))
-    # transfers it to CPU, back to
-    # being a torch.LongTensor
+    # CPU로 다시 전송을 하면, torch.LongTensor로 되돌아옵니다.
