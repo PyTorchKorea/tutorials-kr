cbc17b9dabf5b006a9251aed16b82dca9cd70d70
Junghwan Park <reserve.dev@gmail.com>
2019-06-24 17:35:32 +0900

    'PyTorch로 딥러닝하기: 60분만에 끝장내기' 번역

--- beginner_source/blitz/cifar10_tutorial.py
+++ beginner_source/blitz/cifar10_tutorial.py
@@ -1,36 +1,37 @@
 # -*- coding: utf-8 -*-
 """
-Training a Classifier
-=====================
+분류기(Classifier) 학습하기
+============================
 
-This is it. You have seen how to define neural networks, compute loss and make
-updates to the weights of the network.
+지금까지 어떻게 신경망을 정의하고, 손실을 계산하며 또 가중치를 갱신하는지에
+대해서 배웠습니다.
 
-Now you might be thinking,
+이제 아마도 이런 생각을 하고 계실텐데요,
 
-What about data?
-----------------
+데이터는 어떻게 하나요?
+------------------------
 
-Generally, when you have to deal with image, text, audio or video data,
-you can use standard python packages that load data into a numpy array.
-Then you can convert this array into a ``torch.*Tensor``.
+일반적으로 이미지나 텍스트, 오디오나 비디오 데이터를 다룰 때는 표준 Python 패키지를
+이용하여 NumPy 배열로 불러오면 됩니다. 그 후 그 배열을 ``torch.*Tensor`` 로 변환합니다.
 
--  For images, packages such as Pillow, OpenCV are useful
--  For audio, packages such as scipy and librosa
--  For text, either raw Python or Cython based loading, or NLTK and
-   SpaCy are useful
+-  이미지는 Pillow나 OpenCV 같은 패키지가 유용합니다.
+-  오디오를 처리할 때는 SciPy와 LibROSA가 유용하고요.
+-  텍스트의 경우에는 그냥 Python이나 Cython을 사용해도 되고, NLTK나 SpaCy도
+   유용합니다.
 
-Specifically for vision, we have created a package called
-``torchvision``, that has data loaders for common datasets such as
-Imagenet, CIFAR10, MNIST, etc. and data transformers for images, viz.,
-``torchvision.datasets`` and ``torch.utils.data.DataLoader``.
+특별히 영상 분야를 위한 ``torchvision`` 이라는 패키지가 만들어져 있는데,
+여기에는 Imagenet이나 CIFAR10, MNIST 등과 같이 일반적으로 사용하는 데이터셋을 위한
+데이터 로더(data loader), 즉 ``torchvision.datasets`` 과 이미지용 데이터 변환기
+(data transformer), 즉 ``torch.utils.data.DataLoader`` 가 포함되어 있습니다.
 
-This provides a huge convenience and avoids writing boilerplate code.
+이러한 기능은 엄청나게 편리하며, 매번 유사한 코드(boilerplate code)를 반복해서
+작성하는 것을 피할 수 있습니다.
 
-For this tutorial, we will use the CIFAR10 dataset.
-It has the classes: ‘airplane’, ‘automobile’, ‘bird’, ‘cat’, ‘deer’,
-‘dog’, ‘frog’, ‘horse’, ‘ship’, ‘truck’. The images in CIFAR-10 are of
-size 3x32x32, i.e. 3-channel color images of 32x32 pixels in size.
+이 튜토리얼에서는 CIFAR10 데이터셋을 사용합니다. 여기에는 다음과 같은 분류들이
+있습니다: '비행기(airplane)', '자동차(automobile)', '새(bird)', '고양이(cat)',
+'사슴(deer)', '개(dog)', '개구리(frog)', '말(horse)', '배(ship)', '트럭(truck)'.
+그리고 CIFAR10에 포함된 이미지의 크기는 3x32x32로, 이는 32x32 픽셀 크기의 이미지가
+3개 채널(channel)의 색상로 이뤄져 있다는 것을 뜻합니다.
 
 .. figure:: /_static/img/cifar10.png
    :alt: cifar10
@@ -38,30 +39,30 @@ size 3x32x32, i.e. 3-channel color images of 32x32 pixels in size.
    cifar10
 
 
-Training an image classifier
+이미지 분류기 학습하기
 ----------------------------
 
-We will do the following steps in order:
+다음과 같은 단계로 진행해보겠습니다:
 
-1. Load and normalizing the CIFAR10 training and test datasets using
-   ``torchvision``
-2. Define a Convolutional Neural Network
-3. Define a loss function
-4. Train the network on the training data
-5. Test the network on the test data
+1. ``torchvision`` 을 사용하여 CIFAR10의 학습용 / 시험용 데이터셋을
+   불러오고, 정규화(nomarlizing)합니다.
+2. 합성곱 신경망(Convolution Neural Network)을 정의합니다.
+3. 손실 함수를 정의합니다.
+4. 학습용 데이터를 사용하여 신경망을 학습합니다.
+5. 시험용 데이터를 사용하여 신경망을 검사합니다.
 
-1. Loading and normalizing CIFAR10
+1. CIFAR10를 불러오고 정규화하기
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-Using ``torchvision``, it’s extremely easy to load CIFAR10.
+``torchvision`` 을 사용하여 매우 쉽게 CIFAR10을 불러올 수 있습니다.
 """
 import torch
 import torchvision
 import torchvision.transforms as transforms
 
 ########################################################################
-# The output of torchvision datasets are PILImage images of range [0, 1].
-# We transform them to Tensors of normalized range [-1, 1].
+# torchvision 데이터셋의 출력(output)은 [0, 1] 범위를 갖는 PILImage 이미지입니다.
+# 이를 [-1, 1]의 범위로 정규화된 Tensor로 변환합니다.
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -81,13 +82,12 @@ classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 ########################################################################
-# Let us show some of the training images, for fun.
+# 재미삼아 학습용 이미지 몇 개를 보겠습니다.
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-# functions to show an image
-
+# 이미지를 보여주기 위한 함수
 
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
@@ -96,21 +96,21 @@ def imshow(img):
     plt.show()
 
 
-# get some random training images
+# 학습용 이미지를 무작위로 가져오기
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
-# show images
+# 이미지 보여주기
 imshow(torchvision.utils.make_grid(images))
-# print labels
+# 정답(label) 출력
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 
 ########################################################################
-# 2. Define a Convolutional Neural Network
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# Copy the neural network from the Neural Networks section before and modify it to
-# take 3-channel images (instead of 1-channel images as it was defined).
+# 2. 합성곱 신경망(Convolution Neural Network) 정의하기
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# 이전의 신경망 섹션에서 신경망을 복사한 후, (기존에 1채널 이미지만 처리하도록
+# 정의된 것을) 3채널 이미지를 처리할 수 있도록 수정합니다.
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -139,9 +139,9 @@ class Net(nn.Module):
 net = Net()
 
 ########################################################################
-# 3. Define a Loss function and optimizer
+# 3. 손실 함수와 Optimizer 정의하기
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# Let's use a Classification Cross-Entropy loss and SGD with momentum.
+# 교차 엔트로피 손실(Cross-Entropy loss)과 momentum 값을 갖는 SGD를 사용합니다.
 
 import torch.optim as optim
 
@@ -149,30 +149,30 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 ########################################################################
-# 4. Train the network
+# 4. 신경망 학습하기
 # ^^^^^^^^^^^^^^^^^^^^
 #
-# This is when things start to get interesting.
-# We simply have to loop over our data iterator, and feed the inputs to the
-# network and optimize.
+# 이제 재미있는 부분이 시작됩니다.
+# 단순히 데이터를 반복해서 신경망에 입력으로 제공하고, 최적화(Optimize)만 하면
+# 됩니다.
 
-for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(2):   # 데이터셋을 수차례 반복합니다.
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
+        # [inputs, labels]의 목록인 data로부터 입력을 받은 후;
         inputs, labels = data
 
-        # zero the parameter gradients
+        # 변화도(Gradient) 매개변수를 0으로 만들고
         optimizer.zero_grad()
 
-        # forward + backward + optimize
+        # 순전파 + 역전파 + 최적화를 한 후
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
-        # print statistics
+        # 통계를 출력합니다.
         running_loss += loss.item()
         if i % 2000 == 1999:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
@@ -182,44 +182,42 @@ for epoch in range(2):  # loop over the dataset multiple times
 print('Finished Training')
 
 ########################################################################
-# 5. Test the network on the test data
+# 5. 시험용 데이터로 신경망 검사하기
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-# We have trained the network for 2 passes over the training dataset.
-# But we need to check if the network has learnt anything at all.
+# 지금까지 학습용 데이터셋을 2회 반복하며 신경망을 학습시켰습니다.
+# 신경망이 전혀 배운게 없을지도 모르니 확인해봅니다.
 #
-# We will check this by predicting the class label that the neural network
-# outputs, and checking it against the ground-truth. If the prediction is
-# correct, we add the sample to the list of correct predictions.
+# 신경망이 예측한 출력과 진짜 정답(Ground-truth)을 비교하는 방식으로 확인합니다.
+# 만약 예측이 맞다면 샘플을 '맞은 예측값(correct predictions)' 목록에 넣겠습니다.
 #
-# Okay, first step. Let us display an image from the test set to get familiar.
+# 첫번째로 시험용 데이터를 좀 보겠습니다.
 
 dataiter = iter(testloader)
 images, labels = dataiter.next()
 
-# print images
+# 이미지를 출력합니다.
 imshow(torchvision.utils.make_grid(images))
 print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 ########################################################################
-# Okay, now let us see what the neural network thinks these examples above are:
+# 좋습니다, 이제 이 예제들을 신경망이 어떻게 예측했는지를 보겠습니다:
 
 outputs = net(images)
 
 ########################################################################
-# The outputs are energies for the 10 classes.
-# The higher the energy for a class, the more the network
-# thinks that the image is of the particular class.
-# So, let's get the index of the highest energy:
+# 출력은 10개 분류 각각에 대한 값으로 나타납니다. 어떤 분류에 대해서 더 높은 값이
+# 나타난다는 것은, 신경망이 그 이미지가 해당 분류에 더 가깝다고 생각한다는 것입니다.
+# 따라서, 가장 높은 값을 갖는 인덱스(index)를 뽑아보겠습니다:
 _, predicted = torch.max(outputs, 1)
 
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
                               for j in range(4)))
 
 ########################################################################
-# The results seem pretty good.
+# 결과가 괜찮아보이네요.
 #
-# Let us look at how the network performs on the whole dataset.
+# 그럼 전체 데이터셋에 대해서는 어떻게 동작하는지 보겠습니다.
 
 correct = 0
 total = 0
@@ -235,12 +233,10 @@ print('Accuracy of the network on the 10000 test images: %d %%' % (
     100 * correct / total))
 
 ########################################################################
-# That looks waaay better than chance, which is 10% accuracy (randomly picking
-# a class out of 10 classes).
-# Seems like the network learnt something.
+# (10가지 분류 중에 하나를 무작위로) 찍었을 때의 정확도인 10% 보다는 나아보입니다.
+# 신경망이 뭔가 배우긴 한 것 같네요.
 #
-# Hmmm, what are the classes that performed well, and the classes that did
-# not perform well:
+# 그럼 어떤 것들을 더 잘 분류하고, 어떤 것들을 더 못했는지 알아보겠습니다:
 
 class_correct = list(0. for i in range(10))
 class_total = list(0. for i in range(10))
@@ -261,78 +257,76 @@ for i in range(10):
         classes[i], 100 * class_correct[i] / class_total[i]))
 
 ########################################################################
-# Okay, so what next?
+# 자, 이제 다음으로 무엇을 해볼까요?
 #
-# How do we run these neural networks on the GPU?
+# 이러한 신경망들을 GPU에서 실행하려면 어떻게 해야 할까요?
 #
-# Training on GPU
+# GPU에서 학습하기
 # ----------------
-# Just like how you transfer a Tensor onto the GPU, you transfer the neural
-# net onto the GPU.
+# Tensor를 GPU로 이동했던 것처럼, 신경망 또한 GPU로 옮길 수 있습니다.
 #
-# Let's first define our device as the first visible cuda device if we have
-# CUDA available:
+# 먼저 (CUDA를 사용할 수 있다면) 첫번째 CUDA 장치를 사용하도록 설정합니다:
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# Assuming that we are on a CUDA machine, this should print a CUDA device:
+# CUDA 기기가 존재한다면, 아래 코드가 CUDA 장치를 출력합니다:
 
 print(device)
 
 ########################################################################
-# The rest of this section assumes that ``device`` is a CUDA device.
+# 이 섹션의 나머지 부분에서는 ``device`` 를 CUDA 장치라고 가정하겠습니다.
 #
-# Then these methods will recursively go over all modules and convert their
-# parameters and buffers to CUDA tensors:
+# 그리고 이 메소드(Method)들은 재귀적으로 모든 모듈의 매개변수와 버퍼를
+# CUDA tensor로 변경합니다:
 #
 # .. code:: python
 #
 #     net.to(device)
 #
 #
-# Remember that you will have to send the inputs and targets at every step
-# to the GPU too:
+# 또한, 각 단계에서 입력(input)과 정답(target)도 GPU로 보내야 한다는 것도 기억해야
+# 합니다:
 #
 # .. code:: python
 #
 #         inputs, labels = data[0].to(device), data[1].to(device)
 #
-# Why dont I notice MASSIVE speedup compared to CPU? Because your network
-# is realllly small.
+# CPU와 비교했을 때 어마어마한 속도 차이가 나지 않는 것은 왜 그럴까요?
+# 그 이유는 바로 신경망이 너무 작기 때문입니다.
 #
-# **Exercise:** Try increasing the width of your network (argument 2 of
-# the first ``nn.Conv2d``, and argument 1 of the second ``nn.Conv2d`` –
-# they need to be the same number), see what kind of speedup you get.
+# **연습:** 신경망의 크기를 키워보고, 얼마나 빨라지는지 확인해보세요.
+# (첫번째 ``nn.Conv2d`` 의 2번째 인자와 두번째 ``nn.Conv2d`` 의 1번째 인자는
+# 같은 숫자여야 합니다.)
 #
-# **Goals achieved**:
+# **다음 목표들을 달성했습니다**:
 #
-# - Understanding PyTorch's Tensor library and neural networks at a high level.
-# - Train a small neural network to classify images
+# - 높은 수준에서 PyTorch의 Tensor library와 신경망을 이해합니다.
+# - 이미지를 분류하는 작은 신경망을 학습시킵니다.
 #
-# Training on multiple GPUs
+# 여러개의 GPU에서 학습하기
 # -------------------------
-# If you want to see even more MASSIVE speedup using all of your GPUs,
-# please check out :doc:`data_parallel_tutorial`.
+# 모든 GPU를 활용해서 더욱 더 속도를 올리고 싶다면, :doc:`data_parallel_tutorial`
+# 을 참고하세요.
 #
-# Where do I go next?
-# -------------------
+# 이제 무엇을 해볼까요?
+# -----------------------
 #
 # -  :doc:`Train neural nets to play video games </intermediate/reinforcement_q_learning>`
 # -  `Train a state-of-the-art ResNet network on imagenet`_
 # -  `Train a face generator using Generative Adversarial Networks`_
 # -  `Train a word-level language model using Recurrent LSTM networks`_
-# -  `More examples`_
-# -  `More tutorials`_
-# -  `Discuss PyTorch on the Forums`_
-# -  `Chat with other users on Slack`_
+# -  `다른 예제들 참고하기`_
+# -  `더 많은 튜토리얼 보기`_
+# -  `포럼에서 PyTorch에 대해 얘기하기`_
+# -  `Slack에서 다른 사용자와 대화하기`_
 #
 # .. _Train a state-of-the-art ResNet network on imagenet: https://github.com/pytorch/examples/tree/master/imagenet
 # .. _Train a face generator using Generative Adversarial Networks: https://github.com/pytorch/examples/tree/master/dcgan
 # .. _Train a word-level language model using Recurrent LSTM networks: https://github.com/pytorch/examples/tree/master/word_language_model
-# .. _More examples: https://github.com/pytorch/examples
-# .. _More tutorials: https://github.com/pytorch/tutorials
-# .. _Discuss PyTorch on the Forums: https://discuss.pytorch.org/
-# .. _Chat with other users on Slack: https://pytorch.slack.com/messages/beginner/
+# .. _다른 예제들 참고하기: https://github.com/pytorch/examples
+# .. _더 많은 튜토리얼 보기: https://github.com/pytorch/tutorials
+# .. _포럼에서 PyTorch에 대해 얘기하기: https://discuss.pytorch.org/
+# .. _Slack에서 다른 사용자와 대화하기: http://pytorch.slack.com/messages/beginner/
 
 # %%%%%%INVISIBLE_CODE_BLOCK%%%%%%
 del dataiter
