0b71b66f0e8f27e95d219424ef205d5cf1b984f6
adonisues <adonisues@gmail.com>
2019-08-26 12:17:28 +0900

    intermediate 문자 단위 RNN 이름 생성 완료

--- intermediate_source/char_rnn_generation_tutorial.py
+++ intermediate_source/char_rnn_generation_tutorial.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Generating Names with a Character-Level RNN
+문자-단위 RNN으로 이름 생성하기
 *******************************************
 **Author**: `Sean Robertson <https://github.com/spro/practical-pytorch>`_
+  **번역**: `황성수 <https://github.com/adonisues>`_
 
-In the :doc:`last tutorial </intermediate/char_rnn_classification_tutorial>`
-we used a RNN to classify names into their language of origin. This time
-we'll turn around and generate names from languages.
+:doc:`이전 튜토리얼 </intermediate/char_rnn_classification_tutorial>`
+에서는 이름의 언어를 분류하기 위해 RNN을 사용했습니다.
+이번에는 반대로 언어로 이름을 생성할 예정입니다.
 
 ::
 
@@ -30,48 +31,44 @@ we'll turn around and generate names from languages.
     Hang
     Iun
 
-We are still hand-crafting a small RNN with a few linear layers. The big
-difference is instead of predicting a category after reading in all the
-letters of a name, we input a category and output one letter at a time.
-Recurrently predicting characters to form language (this could also be
-done with words or other higher order constructs) is often referred to
-as a "language model".
+우리는 몇 개의 선형 계층으로 작은 RNN을 직접 만들고 있습니다.
+이전 튜토리얼인 이름을 읽은 후 그 언어를 예측하는 것과의 큰 차이점은 
+언어를 입력하고 한 번에 한 글자를 생성하여 출력하는 것입니다.
+언어 형성(단어 또는 다른 고차원 구조로도 수행 될 수 있음)을 위해 
+문자를 반복적으로 예측하는 것을 "언어 모델" 이라고 합니다. 
 
-**Recommended Reading:**
+**추천 자료:**
 
-I assume you have at least installed PyTorch, know Python, and
-understand Tensors:
+Pytorch를 설치했고, Python을 알고, Tensor를 이해한다고 가정합니다:
 
--  https://pytorch.org/ For installation instructions
--  :doc:`/beginner/deep_learning_60min_blitz` to get started with PyTorch in general
--  :doc:`/beginner/pytorch_with_examples` for a wide and deep overview
--  :doc:`/beginner/former_torchies_tutorial` if you are former Lua Torch user
+-  https://pytorch.org/ 설치 안내
+-  :doc:`/beginner/deep_learning_60min_blitz` PyTorch 시작하기
+-  :doc:`/beginner/pytorch_with_examples` 넓고 깊은 통찰을 위한 자료
+-  :doc:`/beginner/former_torchies_tutorial` 이전 Lua Torch 사용자를 위한 자료
 
-It would also be useful to know about RNNs and how they work:
+RNN과 작동 방식을 아는 것 또한 유용합니다:
 
 -  `The Unreasonable Effectiveness of Recurrent Neural
    Networks <https://karpathy.github.io/2015/05/21/rnn-effectiveness/>`__
-   shows a bunch of real life examples
+   실생활 예제를 보여 줍니다.
 -  `Understanding LSTM
    Networks <https://colah.github.io/posts/2015-08-Understanding-LSTMs/>`__
-   is about LSTMs specifically but also informative about RNNs in
-   general
+   LSTM에 관한 것이지만 RNN에 관해서도 유익합니다.
 
-I also suggest the previous tutorial, :doc:`/intermediate/char_rnn_classification_tutorial`
+이전 튜토리얼도 추천합니다. :doc:`/intermediate/char_rnn_classification_tutorial`
 
 
-Preparing the Data
+데이터 준비
 ==================
 
 .. Note::
-   Download the data from
-   `here <https://download.pytorch.org/tutorial/data.zip>`_
-   and extract it to the current directory.
+   `여기 <https://download.pytorch.org/tutorial/data.zip>`_
+   에서 데이터를 다운 받고, 현재 디렉토리에 압축을 푸십시오.
 
-See the last tutorial for more detail of this process. In short, there
-are a bunch of plain text files ``data/names/[Language].txt`` with a
-name per line. We split lines into an array, convert Unicode to ASCII,
-and end up with a dictionary ``{language: [names ...]}``.
+이 과정의 더 자세한 사항은 지난 튜토리얼을 보십시오.
+요약하면, 줄마다 이름이 적힌 텍스트 파일 ``data/names/[Language].txt`` 있습니다.
+이것을 어레이로 분리하고, Unicode를 ASCII로 변경하고,
+사전 ``{language: [names ...]}`` 을 만들어서 마무리합니다.
 
 """
 from __future__ import unicode_literals, print_function, division
@@ -82,11 +79,11 @@ import unicodedata
 import string
 
 all_letters = string.ascii_letters + " .,;'-"
-n_letters = len(all_letters) + 1 # Plus EOS marker
+n_letters = len(all_letters) + 1 # EOS(end of sentence) 기호 추가
 
 def findFiles(path): return glob.glob(path)
 
-# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
+# 유니코드 문자열을 ASCII로 변환, https://stackoverflow.com/a/518232/2809427
 def unicodeToAscii(s):
     return ''.join(
         c for c in unicodedata.normalize('NFD', s)
@@ -94,12 +91,12 @@ def unicodeToAscii(s):
         and c in all_letters
     )
 
-# Read a file and split into lines
+# 파일을 읽고 줄 단위로 분리
 def readLines(filename):
     lines = open(filename, encoding='utf-8').read().strip().split('\n')
     return [unicodeToAscii(line) for line in lines]
 
-# Build the category_lines dictionary, a list of lines per category
+# 각 언어의 이름 목록인 category_lines 사전 생성
 category_lines = {}
 all_categories = []
 for filename in findFiles('data/names/*.txt'):
@@ -120,25 +117,26 @@ print(unicodeToAscii("O'Néàl"))
 
 
 ######################################################################
-# Creating the Network
+# 네트워크 생성
 # ====================
 #
-# This network extends `the last tutorial's RNN <#Creating-the-Network>`__
-# with an extra argument for the category tensor, which is concatenated
-# along with the others. The category tensor is a one-hot vector just like
-# the letter input.
+# 이 네트워크는 `지난 튜토리얼의 RNN <#Creating-the-Network>`__ 이
+# 다른 입력들과 연결되는 category tensor를 추가 인자로 가지게 확장합니다.
+# category tensor는 문자 입력과 마찬가지로 one-hot 벡터입니다.
 #
-# We will interpret the output as the probability of the next letter. When
-# sampling, the most likely output letter is used as the next input
-# letter.
+# 역자주: 기존 입력과 category tensor를 결합하여 입력으로 사용하기 때문에
+# 입력의 사이즈가 n_categories 만큼 커집니다. 
 #
-# I added a second linear layer ``o2o`` (after combining hidden and
-# output) to give it more muscle to work with. There's also a dropout
-# layer, which `randomly zeros parts of its
-# input <https://arxiv.org/abs/1207.0580>`__ with a given probability
-# (here 0.1) and is usually used to fuzz inputs to prevent overfitting.
-# Here we're using it towards the end of the network to purposely add some
-# chaos and increase sampling variety.
+# 우리는 출력을 다음 문자의 확률로 해석 합니다. 샘플링 할 때,
+# 가장 확률이 높은 문자가 다음 입력 문자로 사용됩니다.
+#
+# 더 나은 동작을 위해 두 번째 선형 레이어
+# ``o2o`` (은닉과 출력을 결합한 후) 를 추가했습니다 .
+# 또한 Drop-out 계층이 있습니다. 이 계층은 주어진 확률(여기서는 0.1)로
+# `무작위로 입력을 0 # <https://arxiv.org/abs/1207.0580>`__ 으로 만듭니다.
+# 일반적으로 입력을 흐리게 해서 과적합을 막는 데 사용됩니다.
+# 여기서 우리는 고의로 일부 혼돈을 추가하고 샘플링 다양성을 높이기
+# 위해 네트워크의 마지막에 이것을 사용합니다.
 #
 # .. figure:: https://i.imgur.com/jzVrf7f.png
 #    :alt:
@@ -174,21 +172,21 @@ class RNN(nn.Module):
 
 
 ######################################################################
-# Training
+# 학습
 # =========
-# Preparing for Training
+# 학습 준비
 # ----------------------
 #
-# First of all, helper functions to get random pairs of (category, line):
+# 제일 먼저 (category, line)의 무작위 쌍을 얻는 함수:
 #
 
 import random
 
-# Random item from a list
+# 목록에서 무작위 아이템 반환
 def randomChoice(l):
     return l[random.randint(0, len(l) - 1)]
 
-# Get a random category and random line from that category
+# 임의의 category 및 그 category에서 무작위 줄(이름) 얻기
 def randomTrainingPair():
     category = randomChoice(all_categories)
     line = randomChoice(category_lines[category])
@@ -196,36 +194,33 @@ def randomTrainingPair():
 
 
 ######################################################################
-# For each timestep (that is, for each letter in a training word) the
-# inputs of the network will be
-# ``(category, current letter, hidden state)`` and the outputs will be
-# ``(next letter, next hidden state)``. So for each training set, we'll
-# need the category, a set of input letters, and a set of output/target
-# letters.
+# 각 시간 단계 마다 (즉, 학습 단어의 각 문자 마다) 네트워크의 입력은
+# ``(언어, 현재 문자, 은닉 상태)`` 가 되고, 출력은
+# ``(다음 문자, 다음 은닉 상태)`` 가 된다. 따라서 각 학습 세트 마다
+# 언어, 입력 문자의 세트, 출력/목표 문자의 세트가 필요하다.
 #
-# Since we are predicting the next letter from the current letter for each
-# timestep, the letter pairs are groups of consecutive letters from the
-# line - e.g. for ``"ABCD<EOS>"`` we would create ("A", "B"), ("B", "C"),
-# ("C", "D"), ("D", "EOS").
+# 각 시간 단계마다 현재 문자에서 다음 문자를 예측하기 때문에,
+# 문자 쌍은 한 줄(하나의 이름)에서 연속된 문자 그룹입니다. - 예를 들어 ``"ABCD<EOS>"`` 는
+# ("A", "B"), ("B", "C"), ("C", "D"), ("D", "EOS") 로 생성합니다.
 #
 # .. figure:: https://i.imgur.com/JH58tXY.png
 #    :alt:
 #
-# The category tensor is a `one-hot
-# tensor <https://en.wikipedia.org/wiki/One-hot>`__ of size
-# ``<1 x n_categories>``. When training we feed it to the network at every
-# timestep - this is a design choice, it could have been included as part
-# of initial hidden state or some other strategy.
+# Category(언어) Tensor는 ``<1 x n_categories>`` 크기의 `One-hot
+# Tensor <https://en.wikipedia.org/wiki/One-hot>`__ 입니다.
+# 학습시에 모든 시간 단계에서 네트워크에 이것을 전달합니다.
+# - 이것은 설계 선택사항으로, 초기 은닉 상태 또는
+# 또 다른 전략의 부분으로 포함될 수 있습니다.
 #
 
-# One-hot vector for category
+# Category를 위한 One-hot 벡터
 def categoryTensor(category):
     li = all_categories.index(category)
     tensor = torch.zeros(1, n_categories)
     tensor[0][li] = 1
     return tensor
 
-# One-hot matrix of first to last letters (not including EOS) for input
+# 입력을 위한 처음부터 마지막 문자(EOS 제외)까지의  One-hot 행렬
 def inputTensor(line):
     tensor = torch.zeros(len(line), 1, n_letters)
     for li in range(len(line)):
@@ -233,20 +228,19 @@ def inputTensor(line):
         tensor[li][0][all_letters.find(letter)] = 1
     return tensor
 
-# LongTensor of second letter to end (EOS) for target
+# 목표를 위한 두번째 문자 부터 마지막(EOS) 까지의 LongTensor
 def targetTensor(line):
     letter_indexes = [all_letters.find(line[li]) for li in range(1, len(line))]
     letter_indexes.append(n_letters - 1) # EOS
     return torch.LongTensor(letter_indexes)
 
-
 ######################################################################
-# For convenience during training we'll make a ``randomTrainingExample``
-# function that fetches a random (category, line) pair and turns them into
-# the required (category, input, target) tensors.
+# 학습 동안 편의를 위해 무작위로 (category[언어], line[이름])을 가져오고
+# 그것을 필요한 형태 (category[언어], input[현재 문자], target[다음 문자]) Tensor로 바꾸는
+# ``randomTrainingExample`` 함수를 만들 예정입니다.
 #
 
-# Make category, input, and target tensors from a random category, line pair
+# 임의의 Category에서 Category, Input, Target Tensor를 만듭니다.
 def randomTrainingExample():
     category, line = randomTrainingPair()
     category_tensor = categoryTensor(category)
@@ -256,15 +250,14 @@ def randomTrainingExample():
 
 
 ######################################################################
-# Training the Network
+# 네트워크 학습
 # --------------------
 #
-# In contrast to classification, where only the last output is used, we
-# are making a prediction at every step, so we are calculating loss at
-# every step.
+# 마지막 출력만 사용하는 분류와 달리, 모든 단계에서 예측을 수행하므로
+# 모든 단계에서 손실을 계산합니다.
 #
-# The magic of autograd allows you to simply sum these losses at each step
-# and call backward at the end.
+# Autograd의 마법이 각 단계의 손실들을 간단하게 합하고 마지막에
+# 역전파를 호출하게 해줍니다.
 #
 
 criterion = nn.NLLLoss()
@@ -293,8 +286,8 @@ def train(category_tensor, input_line_tensor, target_line_tensor):
 
 
 ######################################################################
-# To keep track of how long training takes I am adding a
-# ``timeSince(timestamp)`` function which returns a human readable string:
+# 학습에 걸리는 시간을 추적하기 위해 사람이 읽을 수 있는 문자열을
+# 반환하는``timeSince (timestamp)`` 함수를 추가합니다:
 #
 
 import time
@@ -309,11 +302,11 @@ def timeSince(since):
 
 
 ######################################################################
-# Training is business as usual - call train a bunch of times and wait a
-# few minutes, printing the current time and loss every ``print_every``
-# examples, and keeping store of an average loss per ``plot_every`` examples
-# in ``all_losses`` for plotting later.
-#
+# 학습은 일상적인 일입니다. - 몇번 train() 을 호출하고, 몇 분 정도
+# 기다렸다가 ``print_every`` 마다 현재 시간과 손실을 출력하고,
+# 나중에 도식화를 위해  ``plot_every`` 마다 ``all_losses`` 에
+# 평균 손실을 저장합니다.
+# 
 
 rnn = RNN(n_letters, 128, n_letters)
 
@@ -321,7 +314,7 @@ n_iters = 100000
 print_every = 5000
 plot_every = 500
 all_losses = []
-total_loss = 0 # Reset every plot_every iters
+total_loss = 0 # plot_every 마다 초기화
 
 start = time.time()
 
@@ -338,11 +331,11 @@ for iter in range(1, n_iters + 1):
 
 
 ######################################################################
-# Plotting the Losses
+# 손실 도식화
 # -------------------
 #
-# Plotting the historical loss from all\_losses shows the network
-# learning:
+# all\_losses를 이용한 손실의 도식화는 
+# 네트워크의 학습 상태를 보여줍니다:
 #
 
 import matplotlib.pyplot as plt
@@ -353,35 +346,34 @@ plt.plot(all_losses)
 
 
 ######################################################################
-# Sampling the Network
+# 네트워크 샘플링
 # ====================
 #
-# To sample we give the network a letter and ask what the next one is,
-# feed that in as the next letter, and repeat until the EOS token.
+# 샘플링을 위해서, 네트워크에 하나의 글자를 주고 다음 문자를 물어보고
+# 이것을 다음 문자로 전달하는 것을 EOS 토큰까지 반복합니다.
 #
-# -  Create tensors for input category, starting letter, and empty hidden
-#    state
-# -  Create a string ``output_name`` with the starting letter
-# -  Up to a maximum output length,
+# -  입력 카테고리(언어), 시작 문자, 비어 있는 은닉 상태를 위한 Tensor를 생성하십시오
+# -  시작 문자로 ``output_name`` 문자열을 생성하십시오
+# -  최대 출력 길이까지,
 #
-#    -  Feed the current letter to the network
-#    -  Get the next letter from highest output, and next hidden state
-#    -  If the letter is EOS, stop here
-#    -  If a regular letter, add to ``output_name`` and continue
+#    -  현재 문자를 네트워크에 전달하십시오.
+#    -  가장 높은 출력에서 다음 문자와 다음 은닉 상태를 얻으십시오
+#    -  만일 문자가 EOS면, 여기서 멈추십시오
+#    -  만일 일반적인 문자라면, ``output_name`` 에 추가하고 계속하십시오
 #
-# -  Return the final name
+# -  마지막 이름을 반환하십시오
 #
 # .. Note::
-#    Rather than having to give it a starting letter, another
-#    strategy would have been to include a "start of string" token in
-#    training and have the network choose its own starting letter.
+#    시작 문자를 주는 것 외에 "문자열 시작" 토큰을 학습에
+#    포함되게 하고 네트워크가 자체적으로 시작 문자를 선택하게 하는
+#    다른 방법도 있습니다.
 #
 
 max_length = 20
 
-# Sample from a category and starting letter
+# 카테고리와 시작 문자로 부터 샘플링 하기
 def sample(category, start_letter='A'):
-    with torch.no_grad():  # no need to track history in sampling
+    with torch.no_grad():  # 샘플링에서 히스토리를 추적할 필요 없음
         category_tensor = categoryTensor(category)
         input = inputTensor(start_letter)
         hidden = rnn.initHidden()
@@ -401,7 +393,7 @@ def sample(category, start_letter='A'):
 
         return output_name
 
-# Get multiple samples from one category and multiple starting letters
+# 하나의 카테고리와 여러 시작 문자들로 여러 개의 샘플 얻기
 def samples(category, start_letters='ABC'):
     for start_letter in start_letters:
         print(sample(category, start_letter))
@@ -430,5 +422,5 @@ samples('Chinese', 'CHI')
 # -  Get better results with a bigger and/or better shaped network
 #
 #    -  Try the nn.LSTM and nn.GRU layers
-#    -  Combine multiple of these RNNs as a higher level network
+#    -  상위 수준 네트워크로 여러 개의 이런 RNN을  결합해 보십시오
 #
