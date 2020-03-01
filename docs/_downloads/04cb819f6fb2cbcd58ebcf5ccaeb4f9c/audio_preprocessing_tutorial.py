253489570f8d3d7b0f7a8c5d2c7b2b5b8c8d3212
Junghwan Park <reserve.dev@gmail.com>
2020-03-01 17:11:42 +0900

    Rebuild

--- docs/_downloads/04cb819f6fb2cbcd58ebcf5ccaeb4f9c/audio_preprocessing_tutorial.py
+++ docs/_downloads/04cb819f6fb2cbcd58ebcf5ccaeb4f9c/audio_preprocessing_tutorial.py
@@ -1,16 +1,19 @@
 """
-torchaudio 튜토리얼
+torchaudio Tutorial
 ===================
 
+PyTorch is an open source deep learning platform that provides a
+seamless path from research prototyping to production deployment with
+GPU support.
 
-PyTorch는 GPU를 지원하는 연구 프로토타이핑에서
-상품 배포까지 끊김없이 지원하는 오픈 소스 딥 러닝 플랫폼입니다.
+Significant effort in solving machine learning problems goes into data
+preparation. ``torchaudio`` leverages PyTorch’s GPU support, and provides
+many tools to make data loading easy and more readable. In this
+tutorial, we will see how to load and preprocess data from a simple
+dataset.
 
-머신 러닝 문제 해결에는 많은 노력을 데이터 준비에 씁니다.
-``torchaudio`` 는 PyTorch의 GPU 지원을 활용하고, 데이터 로드를 더 쉽고 읽기 쉽게 해주는 많은 도구를 제공합니다.
-이 튜토리얼에서는 간단한 데이터 세트에서 데이터를 로드하고 전처리하는 방법을 살펴 봅니다.
-
-이 튜토리얼에서 더 쉬운 시각화를 위한 ``matplotlib`` 패키지가 설치되어 있는지 확인하십시오.
+For this tutorial, please make sure the ``matplotlib`` package is
+installed for easier visualization.
 
 """
 
@@ -18,17 +21,13 @@ import torch
 import torchaudio
 import matplotlib.pyplot as plt
 
-
 ######################################################################
-# 데이터 세트 열기
-# ---------------------
-#
-
-
-######################################################################
-# torchaudio는 wav 및 mp3 형식의 사운드 파일 로드를 지원합니다.
-# 우리는 결과 원시 오디오 신호(raw audio signal)를 파형(waveform)이라고 부릅니다.
-#
+# Opening a file
+# -----------------
+# 
+# ``torchaudio`` also supports loading sound files in the wav and mp3 format. We
+# call waveform the resulting raw audio signal.
+# 
 
 filename = "../_static/img/steam-train-whistle-daniel_simon-converted-from-mp3.wav"
 waveform, sample_rate = torchaudio.load(filename)
@@ -39,31 +38,51 @@ print("Sample rate of waveform: {}".format(sample_rate))
 plt.figure()
 plt.plot(waveform.t().numpy())
 
+######################################################################
+# When you load a file in ``torchaudio``, you can optionally specify the backend to use either 
+# `SoX <https://pypi.org/project/sox/>`_ or `SoundFile <https://pypi.org/project/SoundFile/>`_ 
+# via ``torchaudio.set_audio_backend``. These backends are loaded lazily when needed.
+# 
+# ``torchaudio`` also makes JIT compilation optional for functions, and uses ``nn.Module`` where possible.
 
 ######################################################################
-# 변환
+# Transformations
 # ---------------
+# 
+# ``torchaudio`` supports a growing list of
+# `transformations <https://pytorch.org/audio/transforms.html>`_.
+# 
+# -  **Resample**: Resample waveform to a different sample rate.
+# -  **Spectrogram**: Create a spectrogram from a waveform.
+# -  **GriffinLim**: Compute waveform from a linear scale magnitude spectrogram using 
+#    the Griffin-Lim transformation.
+# -  **ComputeDeltas**: Compute delta coefficients of a tensor, usually a spectrogram.
+# -  **ComplexNorm**: Compute the norm of a complex tensor.
+# -  **MelScale**: This turns a normal STFT into a Mel-frequency STFT,
+#    using a conversion matrix.
+# -  **AmplitudeToDB**: This turns a spectrogram from the
+#    power/amplitude scale to the decibel scale.
+# -  **MFCC**: Create the Mel-frequency cepstrum coefficients from a
+#    waveform.
+# -  **MelSpectrogram**: Create MEL Spectrograms from a waveform using the
+#    STFT function in PyTorch.
+# -  **MuLawEncoding**: Encode waveform based on mu-law companding.
+# -  **MuLawDecoding**: Decode mu-law encoded waveform.
+# -  **TimeStretch**: Stretch a spectrogram in time without modifying pitch for a given rate.
+# -  **FrequencyMasking**: Apply masking to a spectrogram in the frequency domain.
+# -  **TimeMasking**: Apply masking to a spectrogram in the time domain.
 #
-# torchaudio 점점 더 많은
-# `변환 <https://pytorch.org/audio/transforms.html>`_ 을 지원 합니다..
-#
-# -  **Resample**: 다른 샘플링 레이트로 파형을 리샘플.
-# -  **Spectrogram**: 파형에서 스펙트로그램 생성.
-# -  **MelScale**: 변환 행렬을 이용해 일반 STFT를 Mel-frequency STFT로 변환.
-# -  **AmplitudeToDB**: 스펙트로그램을 파워/크기 단위에서 데시벨 단위로 변환.
-# -  **MFCC**: 파형에서 Mel-frequency cepstrum coefficients 생성.
-# -  **MelSpectrogram**: Pytorch의 STFT 함수를 이용해 파형에서 MEL Spectrograms 생성.
-# -  **MuLawEncoding**: mu-law 압신 기반 파형 인코딩.
-# -  **MuLawDecoding**: mu-law 인코딩된 파형 디코딩.
-#
-# 모든 변환은 nn.Modules 또는 jit.ScriptModules 이므로,
-# 언제든지 신경망의 일부로 사용할 수 있습니다.
-#
+# Each transform supports batching: you can perform a transform on a single raw 
+# audio signal or spectrogram, or many of the same shape.
+# 
+# Since all transforms are ``nn.Modules`` or ``jit.ScriptModules``, they can be
+# used as part of a neural network at any point.
+# 
 
 
 ######################################################################
-# 처음으로 로그 스케일 스펙트로그램을 볼 수 있습니다.
-#
+# To start, we can look at the log of the spectrogram on a log scale.
+# 
 
 specgram = torchaudio.transforms.Spectrogram()(waveform)
 
@@ -74,8 +93,8 @@ plt.imshow(specgram.log2()[0,:,:].numpy(), cmap='gray')
 
 
 ######################################################################
-# 또는 로그 스케일의  Mel Spectrogram 을 볼 수 있습니다..
-#
+# Or we can look at the Mel Spectrogram on a log scale.
+# 
 
 specgram = torchaudio.transforms.MelSpectrogram()(waveform)
 
@@ -86,12 +105,12 @@ p = plt.imshow(specgram.log2()[0,:,:].detach().numpy(), cmap='gray')
 
 
 ######################################################################
-# 한번의 하나의 채널 씩 파형을 리샘플 할 수 있습니다.
-#
+# We can resample the waveform, one channel at a time.
+# 
 
 new_sample_rate = sample_rate/10
 
-# 리샘플이 단일 채널에 적용되기 때문에 우리는 여기서 첫번째 채널을 리샘플 합니다
+# Since Resample applies to a single channel, we resample first channel here
 channel = 0
 transformed = torchaudio.transforms.Resample(sample_rate, new_sample_rate)(waveform[channel,:].view(1,-1))
 
@@ -102,31 +121,33 @@ plt.plot(transformed[0,:].numpy())
 
 
 ######################################################################
-# 변형의 다른 예시로  Mu-Law 인코딩으로 신호를 인코딩 할 수 있습니다.
-# 그러나 그렇게 하기 위해서, 신호가 -1과 1 사이가 되도록 하는 것이 필요합니다.
-# 그 Tensor 는 일반적인 Pytorch Tensor이므로 표준 동작을 적용할 수 있습니다.
-#
+# As another example of transformations, we can encode the signal based on
+# Mu-Law enconding. But to do so, we need the signal to be between -1 and
+# 1. Since the tensor is just a regular PyTorch tensor, we can apply
+# standard operators on it.
+# 
 
-# Tensor가 [-1,1] 사이인지 확인해 봅시다.
+# Let's check if the tensor is in the interval [-1,1]
 print("Min of waveform: {}\nMax of waveform: {}\nMean of waveform: {}".format(waveform.min(), waveform.max(), waveform.mean()))
 
 
 ######################################################################
-# 파형이 이미 -1과 1 사이에 있기 때문에 정규화를 할 필요가 없습니다.
-#
+# Since the waveform is already between -1 and 1, we do not need to
+# normalize it.
+# 
 
 def normalize(tensor):
-    # 평균을 빼고,  [-1,1] 간격으로 크기를 맞춥니다.
+    # Subtract the mean, and scale to the interval [-1,1]
     tensor_minusmean = tensor - tensor.mean()
     return tensor_minusmean/tensor_minusmean.abs().max()
 
-# 전체 간격 [-1,1]로 정규화 합니다.
+# Let's normalize to the full interval [-1,1]
 # waveform = normalize(waveform)
 
 
 ######################################################################
-# 파형을 인코팅해 봅시다.
-#
+# Let’s apply encode the waveform.
+# 
 
 transformed = torchaudio.transforms.MuLawEncoding()(waveform)
 
@@ -137,8 +158,8 @@ plt.plot(transformed[0,:].numpy())
 
 
 ######################################################################
-# 이제 디코딩을 합니다.
-#
+# And now decode.
+# 
 
 reconstructed = torchaudio.transforms.MuLawDecoding()(transformed)
 
@@ -149,32 +170,109 @@ plt.plot(reconstructed[0,:].numpy())
 
 
 ######################################################################
-# 마침내 원래 파형과 재구축된 버전을 비교할 수 있습니다.
-#
+# We can finally compare the original waveform with its reconstructed
+# version.
+# 
 
-# 중앙값 상대 차이(median relative difference) 계산
+# Compute median relative difference
 err = ((waveform-reconstructed).abs() / waveform.abs()).median()
 
 print("Median relative difference between original and MuLaw reconstucted signals: {:.2%}".format(err))
 
 
 ######################################################################
-# Kaldi에서 torchaudio로 변경
+# Functional
+# ---------------
+# 
+# The transformations seen above rely on lower level stateless functions for their computations. 
+# These functions are available under ``torchaudio.functional``. The complete list is available 
+# `here <https://pytorch.org/audio/functional.html>`_ and includes:
+#
+# -  **istft**: Inverse short time Fourier Transform.
+# -  **gain**: Applies amplification or attenuation to the whole waveform.
+# -  **dither**: Increases the perceived dynamic range of audio stored at a
+#    particular bit-depth.
+# -  **compute_deltas**: Compute delta coefficients of a tensor.
+# -  **equalizer_biquad**: Design biquad peaking equalizer filter and perform filtering.
+# -  **lowpass_biquad**: Design biquad lowpass filter and perform filtering.
+# -  **highpass_biquad**:Design biquad highpass filter and perform filtering.
+# 
+# For example, let's try the `mu_law_encoding` functional:
+
+mu_law_encoding_waveform = torchaudio.functional.mu_law_encoding(waveform, quantization_channels=256)
+
+print("Shape of transformed waveform: {}".format(mu_law_encoding_waveform.size()))
+
+plt.figure()
+plt.plot(mu_law_encoding_waveform[0,:].numpy())
+
+######################################################################
+# You can see how the output fron ``torchaudio.functional.mu_law_encoding`` is the same as 
+# the output from ``torchaudio.transforms.MuLawEncoding``.
+#
+# Now let's experiment with a few of the other functionals and visualize their output. Taking our 
+# spectogram, we can compute it's deltas:
+
+computed = torchaudio.functional.compute_deltas(specgram, win_length=3)
+print("Shape of computed deltas: {}".format(computed.shape))
+
+plt.figure()
+plt.imshow(computed.log2()[0,:,:].detach().numpy(), cmap='gray')
+
+######################################################################
+# We can take the original waveform and apply different effects to it.
+#
+
+gain_waveform = torchaudio.functional.gain(waveform, gain_db=5.0)
+print("Min of gain_waveform: {}\nMax of gain_waveform: {}\nMean of gain_waveform: {}".format(gain_waveform.min(), gain_waveform.max(), gain_waveform.mean()))
+
+dither_waveform = torchaudio.functional.dither(waveform)
+print("Min of dither_waveform: {}\nMax of dither_waveform: {}\nMean of dither_waveform: {}".format(dither_waveform.min(), dither_waveform.max(), dither_waveform.mean()))
+
+######################################################################
+# Another example of the capabilities in ``torchaudio.functional`` are applying filters to our
+# waveform. Applying the lowpass biquad filter to our waveform will output a new waveform with 
+# the signal of the frequency modified.
+
+lowpass_waveform = torchaudio.functional.lowpass_biquad(waveform, sample_rate, cutoff_freq=3000)
+
+print("Min of lowpass_waveform: {}\nMax of lowpass_waveform: {}\nMean of lowpass_waveform: {}".format(lowpass_waveform.min(), lowpass_waveform.max(), lowpass_waveform.mean()))
+
+plt.figure()
+plt.plot(lowpass_waveform.t().numpy())
+
+######################################################################
+# We can also visualize a waveform with the highpass biquad filter.
+# 
+
+highpass_waveform = torchaudio.functional.highpass_biquad(waveform, sample_rate, cutoff_freq=2000)
+
+print("Min of highpass_waveform: {}\nMax of highpass_waveform: {}\nMean of highpass_waveform: {}".format(highpass_waveform.min(), highpass_waveform.max(), highpass_waveform.mean()))
+
+plt.figure()
+plt.plot(highpass_waveform.t().numpy())
+
+
+######################################################################
+# Migrating to torchaudio from Kaldi
 # ----------------------------------
-#
-# 음성인식 툴킷 `Kaldi <http://github.com/kaldi-asr/kaldi>`_ 에 익숙한 사용자를 위해,
-# torchaudio는  ``torchaudio.kaldi_io`` 로 호환성을 제공합니다.
-# 그것은 kaldi scp 또는 ark 파일 또는 streams 를 아래 함수로 읽을 수 있습니다:
-#
+# 
+# Users may be familiar with
+# `Kaldi <http://github.com/kaldi-asr/kaldi>`_, a toolkit for speech
+# recognition. ``torchaudio`` offers compatibility with it in
+# ``torchaudio.kaldi_io``. It can indeed read from kaldi scp, or ark file
+# or streams with:
+# 
 # -  read_vec_int_ark
 # -  read_vec_flt_scp
 # -  read_vec_flt_arkfile/stream
 # -  read_mat_scp
 # -  read_mat_ark
-#
-# torchaudio 는 ``spectrogram`` 과 ``fbank`` 를 위해 GPU의 장점을 가진 Kaldi 호환 변형을 제공합니다.
-# 더 많은 정보를 위해서 `여기 <compliance.kaldi.html>`__ 를 보십시오.
-#
+# 
+# ``torchaudio`` provides Kaldi-compatible transforms for ``spectrogram``,
+# ``fbank``, ``mfcc``, and ``resample_waveform with the benefit of GPU support, see
+# `here <compliance.kaldi.html>`__ for more information.
+# 
 
 n_fft = 400.0
 frame_length = n_fft / sample_rate * 1000.0
@@ -200,8 +298,9 @@ plt.imshow(specgram.t().numpy(), cmap='gray')
 
 
 ######################################################################
-# 또한 Kaldi의 구현과 동일한 필터 뱅크 특징 계산을 지원합니다.
-#
+# We also support computing the filterbank features from waveforms,
+# matching Kaldi’s implementation.
+# 
 
 fbank = torchaudio.compliance.kaldi.fbank(waveform, **params)
 
@@ -212,12 +311,65 @@ plt.imshow(fbank.t().numpy(), cmap='gray')
 
 
 ######################################################################
-# 결론
+# You can create mel frequency cepstral coefficients from a raw audio signal
+# This matches the input/output of Kaldi’s compute-mfcc-feats.
+# 
+
+mfcc = torchaudio.compliance.kaldi.mfcc(waveform, **params)
+
+print("Shape of mfcc: {}".format(mfcc.size()))
+
+plt.figure()
+plt.imshow(mfcc.t().numpy(), cmap='gray')
+
+
+######################################################################
+# Available Datasets
+# -----------------
+# 
+# If you do not want to create your own dataset to train your model, ``torchaudio`` offers a
+# unified dataset interface. This interface supports lazy-loading of files to memory, download 
+# and extract functions, and datasets to build models.
+# 
+# The datasets ``torchaudio`` currently supports are:
+#
+# -  **VCTK**: Speech data uttered by 109 native speakers of English with various accents
+#    (`Read more here <https://homepages.inf.ed.ac.uk/jyamagis/page3/page58/page58.html>`_).
+# -  **Yesno**: Sixty recordings of one individual saying yes or no in Hebrew; each
+#    recording is eight words long (`Read more here <https://www.openslr.org/1/>`_).
+# -  **Common Voice**: An open source, multi-language dataset of voices that anyone can use
+#    to train speech-enabled applications (`Read more here <https://voice.mozilla.org/en/datasets>`_).
+# -  **LibriSpeech**: Large-scale (1000 hours) corpus of read English speech (`Read more here <http://www.openslr.org/12>`_).
+# 
+
+yesno_data = torchaudio.datasets.YESNO('./', download=True)
+
+# A data point in Yesno is a tuple (waveform, sample_rate, labels) where labels is a list of integers with 1 for yes and 0 for no.
+
+# Pick data point number 3 to see an example of the the yesno_data:
+n = 3
+waveform, sample_rate, labels = yesno_data[n]
+
+print("Waveform: {}\nSample rate: {}\nLabels: {}".format(waveform, sample_rate, labels))
+
+plt.figure()
+plt.plot(waveform.t().numpy())
+
+
+######################################################################
+# Now, whenever you ask for a sound file from the dataset, it is loaded in memory only when you ask for it.
+# Meaning, the dataset only loads and keeps in memory the items that you want and use, saving on memory.
+#
+
+######################################################################
+# Conclusion
 # ----------
-#
-# 원시 오디오 신호 또는 파형을 이용해서 torchaudio로 오디오 파일여는 방법과
-# 그 파형을 전처리하고 변형하는 방법을 설명했습니다.
-# Pytorch에 torchaudio 가 포함되어있기 때문에,
-# 이 기술들은 GPU를 활용한 상태로 음성인식과 같은 더 발전된
-# 오디오 어플리케이션의 블락을 구축하는데 사용될 수 있습니다.
-#
+# 
+# We used an example raw audio signal, or waveform, to illustrate how to
+# open an audio file using ``torchaudio``, and how to pre-process,
+# transform, and apply functions to such waveform. We also demonstrated how
+# to use familiar Kaldi functions, as well as utilize built-in datasets to 
+# construct our models. Given that ``torchaudio`` is built on PyTorch,
+# these techniques can be used as building blocks for more advanced audio
+# applications, such as speech recognition, while leveraging GPUs.
+# 
