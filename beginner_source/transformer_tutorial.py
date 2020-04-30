286373816cd61c30316bc1fc62b8fda8d107812c
Sangho.Nam <namdori61@gmail.com>
2020-04-30 09:51:23 +0900

    Translate transformer_tutorial

--- beginner_source/transformer_tutorial.py
+++ beginner_source/transformer_tutorial.py
@@ -1,47 +1,45 @@
 """
-Sequence-to-Sequence Modeling with nn.Transformer and TorchText
+nn.Transformer 와 TorchText 로 시퀀스-투-시퀀스(Sequence-to-Sequence) 모델링
 ===============================================================
 
-This is a tutorial on how to train a sequence-to-sequence model
-that uses the
-`nn.Transformer <https://pytorch.org/docs/master/nn.html?highlight=nn%20transformer#torch.nn.Transformer>`__ module.
+이 튜토리얼에서는
+`nn.Transformer <https://pytorch.org/docs/master/nn.html?highlight=nn%20transformer#torch.nn.Transformer>`__ 모듈을
+이용하는 시퀀스-투-시퀀스 모델을 학습하는 방법을 배워보겠습니다.
 
-PyTorch 1.2 release includes a standard transformer module based on the
-paper `Attention is All You
-Need <https://arxiv.org/pdf/1706.03762.pdf>`__. The transformer model
-has been proved to be superior in quality for many sequence-to-sequence
-problems while being more parallelizable. The ``nn.Transformer`` module
-relies entirely on an attention mechanism (another module recently
-implemented as `nn.MultiheadAttention <https://pytorch.org/docs/master/nn.html?highlight=multiheadattention#torch.nn.MultiheadAttention>`__) to draw global dependencies
-between input and output. The ``nn.Transformer`` module is now highly
-modularized such that a single component (like `nn.TransformerEncoder <https://pytorch.org/docs/master/nn.html?highlight=nn%20transformerencoder#torch.nn.TransformerEncoder>`__
-in this tutorial) can be easily adapted/composed.
+PyTorch 1.2 버젼 배포는
+`Attention is All You Need <https://arxiv.org/pdf/1706.03762.pdf>`__ 논문에
+기반한 표준 트랜스포머 (transformer) 모듈을 포함하고 있습니다.
+트랜스포머 모델은 더 높은 수준의 병렬화 (parallelization)가 가능하면서도
+많은 시퀀스-투-시퀀스 문제들에서 성능이 우월하다는 것이 증명되어 왔습니다.
+``nn.Transformer`` 모듈은 입력 (input) 과 출력 (output) 사이의 전역적인 의존성 (global dependencies)
+을 나타내기 위하여 전적으로 어텐션 (attention) 메커니즘에 의존합니다.
+(최근에 또 다른 모듈이 `nn.MultiheadAttention <https://pytorch.org/docs/master/nn.html?highlight=multiheadattention#torch.nn.MultiheadAttention>`__ 으로 구현되었습니다.)
+``nn.Transformer`` 모듈은 현재 고도로 모듈화되어 있어,
+다음과 같은 단일 컴포넌트 (이 튜토리얼의 `nn.TransformerEncoder <https://pytorch.org/docs/master/nn.html?highlight=nn%20transformerencoder#torch.nn.TransformerEncoder>`__ 와 같은)
+는 쉽게 적용 및 구성될 수 있습니다.
 
 .. image:: ../_static/img/transformer_architecture.jpg
 
 """
 
 ######################################################################
-# Define the model
+# 모델 정의하기
 # ----------------
 #
 
 
 ######################################################################
-# In this tutorial, we train ``nn.TransformerEncoder`` model on a
-# language modeling task. The language modeling task is to assign a
-# probability for the likelihood of a given word (or a sequence of words)
-# to follow a sequence of words. A sequence of tokens are passed to the embedding
-# layer first, followed by a positional encoding layer to account for the order
-# of the word (see the next paragraph for more details). The
-# ``nn.TransformerEncoder`` consists of multiple layers of
-# `nn.TransformerEncoderLayer <https://pytorch.org/docs/master/nn.html?highlight=transformerencoderlayer#torch.nn.TransformerEncoderLayer>`__. Along with the input sequence, a square
-# attention mask is required because the self-attention layers in
-# ``nn.TransformerEncoder`` are only allowed to attend the earlier positions in
-# the sequence. For the language modeling task, any tokens on the future
-# positions should be masked. To have the actual words, the output
-# of ``nn.TransformerEncoder`` model is sent to the final Linear
-# layer, which is followed by a log-Softmax function.
+# 이 튜토리얼에서, 우리는 ``nn.TransformerEncoder`` 모델을 언어 모델링 (language modeling) 과제에 대해서 학습시킬 것입니다.
+# 언어 모델링 과제는 주어진 단어 (또는 단어의 시퀀스) 가 다음에 이어지는 단어 시퀀스를 따를 가능성 (likelihood)에 대한 확률을 할당하는 것입니다.
+# 먼저, 토큰 (token) 들의 시퀀스가 임베딩 (embedding) 레이어로 전달되며, 이어서 포지셔널 인코딩 (positional encoding) 레이어가 각 단어의 순서를 설명합니다.
+# (더 자세한 설명은 다음 단락을 참고해주세요.)
+# ``nn.TransformerEncoder`` 는 여러 개의
+# `nn.TransformerEncoderLayer <https://pytorch.org/docs/master/nn.html?highlight=transformerencoderlayer#torch.nn.TransformerEncoderLayer>`__
+# 레이어로 구성되어 있습니다.
+# ``nn.TransformerEncoder`` 내부의 셀프-어텐션 (self-attention) 레이어들은 시퀀스 안에서의 이전 포지션에만 집중하도록 허용되기 때문에,
+# 입력 (input) 순서와 함께, 정사각 형태의 어텐션 마스크 (attention mask) 가 필요합니다.
+# 언어 모델링 과제를 위해서, 미래의 포지션에 있는 모든 토큰들은 마스킹 되어야 (가려져야) 합니다.
+# 실제 단어를 얻기 위해서, ``nn.TransformerEncoder`` 의 출력은 로그-소프트맥스 (log-Softmax) 로 이어지는 최종 선형 (Linear) 레이어로 전달 됩니다.
 #
 
 import math
@@ -90,11 +88,9 @@ class TransformerModel(nn.Module):
 
 
 ######################################################################
-# ``PositionalEncoding`` module injects some information about the
-# relative or absolute position of the tokens in the sequence. The
-# positional encodings have the same dimension as the embeddings so that
-# the two can be summed. Here, we use ``sine`` and ``cosine`` functions of
-# different frequencies.
+# ``PositionalEncoding`` 모듈은 시퀀스 안에서 토큰의 상대적인 또는 절대적인 포지션에 대한 어떤 정보를 주입합니다.
+# 포지셔널 인코딩은 임베딩과 합칠 수 있도록 똑같은 차원을 가집니다.
+# 여기에서, 우리는 다른 주파수 (frequency) 의 ``sine`` 과 ``cosine`` 함수를 사용합니다.
 #
 
 class PositionalEncoding(nn.Module):
@@ -117,20 +113,16 @@ class PositionalEncoding(nn.Module):
 
 
 ######################################################################
-# Load and batch data
+# 데이터 로드하고 배치 만들기
 # -------------------
 #
 
 
 ######################################################################
-# The training process uses Wikitext-2 dataset from ``torchtext``. The
-# vocab object is built based on the train dataset and is used to numericalize
-# tokens into tensors. Starting from sequential data, the ``batchify()``
-# function arranges the dataset into columns, trimming off any tokens remaining
-# after the data has been divided into batches of size ``batch_size``.
-# For instance, with the alphabet as the sequence (total length of 26)
-# and a batch size of 4, we would divide the alphabet into 4 sequences of
-# length 6:
+# 학습 과정에서는 ``torchtext`` 의 Wikitext-2 데이터셋을 이용합니다.
+# 단어 오브젝트는 훈련 데이터셋 (train dataset) 에 의하여 만들어지고, 토큰을 텐서 (tensor)로 수치화하는데 사용됩니다.
+# 시퀀스 데이터로부터 시작하여, ``batchify()`` 함수는 데이터셋을 컬럼들로 배열하고, ``batch_size`` 사이즈의 배치들로 나눈 후에 남은 모든 토큰을 버립니다.
+# 예를 들어, 알파벳을 시퀀스 (총 길이 26) 로 생각하고 배치 사이즈를 4라고 한다면, 우리는 알파벳을 길이가 6인 4개의 시퀀스로 나눌 수 있습니다.
 #
 # .. math::
 #   \begin{bmatrix}
@@ -144,9 +136,7 @@ class PositionalEncoding(nn.Module):
 #   \begin{bmatrix}\text{S} \\ \text{T} \\ \text{U} \\ \text{V} \\ \text{W} \\ \text{X}\end{bmatrix}
 #   \end{bmatrix}
 #
-# These columns are treated as independent by the model, which means that
-# the dependence of ``G`` and ``F`` can not be learned, but allows more
-# efficient batch processing.
+# 이 컬럼들은 모델에 의해서 독립적으로 취급되며, 이것은 더 효율적인 배치 프로세싱 (batch processing) 이 가능하지만, ``G`` 와 ``F`` 의 의존성이 학습될 수 없다는 것을 의미합니다.
 #
 
 import torchtext
@@ -161,11 +151,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def batchify(data, bsz):
     data = TEXT.numericalize([data.examples[0].text])
-    # Divide the dataset into bsz parts.
+    # 데이터셋을 bsz 파트들로 나눕니다.
     nbatch = data.size(0) // bsz
-    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    # 깔끔하게 나누어 떨어지지 않는 추가적인 부분 (나머지들) 은 잘라냅니다.
     data = data.narrow(0, 0, nbatch * bsz)
-    # Evenly divide the data across the bsz batches.
+    # 데이터에 대하여 bsz 배치들로 동등하게 나눕니다.
     data = data.view(bsz, -1).t().contiguous()
     return data.to(device)
 
@@ -177,23 +167,21 @@ test_data = batchify(test_txt, eval_batch_size)
 
 
 ######################################################################
-# Functions to generate input and target sequence
+# 입력 (input) 과 타겟 (target) 시퀀스를 생성하기 위한 함수들
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
 
 ######################################################################
-# ``get_batch()`` function generates the input and target sequence for
-# the transformer model. It subdivides the source data into chunks of
-# length ``bptt``. For the language modeling task, the model needs the
-# following words as ``Target``. For example, with a ``bptt`` value of 2,
-# we’d get the following two Variables for ``i`` = 0:
+# ``get_batch()`` 함수는 트랜스포머 모델을 위한 입력과 타겟 시퀀스를 생성합니다.
+# 이 함수는 소스 데이터를 ``bptt`` 길이를 가진 덩어리로 세분화 합니다.
+# 언어 모델링 과제를 위해서, 모델은 다음 단어인 ``Target`` 이 필요 합니다.
+# 예를 들어, ``bptt`` 의 값이 2 라면, 우리는 ``i`` = 0 일 때 다음의 2 개의 변수 (Variable) 를 얻을 수 있습니다:
 #
 # .. image:: ../_static/img/transformer_input_target.png
 #
-# It should be noted that the chunks are along dimension 0, consistent
-# with the ``S`` dimension in the Transformer model. The batch dimension
-# ``N`` is along dimension 1.
+# 변수 덩어리는 트랜스포머 모델의 ``S`` 차원과 일치하는 0 차원에 해당합니다.
+# 배치 차원 ``N`` 은 1 차원에 해당합니다.
 #
 
 bptt = 35
@@ -205,51 +193,53 @@ def get_batch(source, i):
 
 
 ######################################################################
-# Initiate an instance
+# 인스턴스 (instance) 초기화 하기
 # --------------------
 #
 
 
 ######################################################################
-# The model is set up with the hyperparameter below. The vocab size is
-# equal to the length of the vocab object.
+# 모델은 아래와 같은 하이퍼파라미터 (hyperparameter) 로 세팅 됩니다.
+# 단어 사이즈는 단어 오브젝트의 길이와 일치 합니다.
 #
 
-ntokens = len(TEXT.vocab.stoi) # the size of vocabulary
-emsize = 200 # embedding dimension
-nhid = 200 # the dimension of the feedforward network model in nn.TransformerEncoder
-nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
-nhead = 2 # the number of heads in the multiheadattention models
-dropout = 0.2 # the dropout value
+ntokens = len(TEXT.vocab.stoi) # 단어 사전의 크기
+emsize = 200 # 임베딩 차원
+nhid = 200 # nn.TransformerEncoder 에서 피드포워드 네트워크 (feedforward network) 모델의 차원
+nlayers = 2 # nn.TransformerEncoder 내부의 nn.TransformerEncoderLayer 개수
+nhead = 2 # 멀티헤드 어텐션 (multi-head attention) 모델의 헤드 개수
+dropout = 0.2 # 드랍아웃 (dropout) 값
 model = TransformerModel(ntokens, emsize, nhead, nhid, nlayers, dropout).to(device)
 
 
 ######################################################################
-# Run the model
+# 모델 실행하기
 # -------------
 #
 
 
 ######################################################################
+# 손실 (loss) 을 추적하는 데에는
 # `CrossEntropyLoss <https://pytorch.org/docs/master/nn.html?highlight=crossentropyloss#torch.nn.CrossEntropyLoss>`__
-# is applied to track the loss and
+# 가 적용되며, 옵티마이저 (optimizer) 로서
 # `SGD <https://pytorch.org/docs/master/optim.html?highlight=sgd#torch.optim.SGD>`__
-# implements stochastic gradient descent method as the optimizer. The initial
-# learning rate is set to 5.0. `StepLR <https://pytorch.org/docs/master/optim.html?highlight=steplr#torch.optim.lr_scheduler.StepLR>`__ is
-# applied to adjust the learn rate through epochs. During the
-# training, we use
+# 는 확률적 경사 하강법 (stochastic gradient descent method) 을 구현합니다.
+# 초기 학습률 (learning rate) 은 5.0 으로 설정됩니다.
+# `StepLR <https://pytorch.org/docs/master/optim.html?highlight=steplr#torch.optim.lr_scheduler.StepLR>`__
+# 은 에포크 (epoch) 에 따라서 학습률을 조절하는데 사용됩니다.
+# 학습하는 동안에, 우리는 기울기 폭발 (gradient exploding) 을 방지하기 위하여 모든 기울기를 함께 스케일 (scale) 하는 함수인
 # `nn.utils.clip_grad_norm\_ <https://pytorch.org/docs/master/nn.html?highlight=nn%20utils%20clip_grad_norm#torch.nn.utils.clip_grad_norm_>`__
-# function to scale all the gradient together to prevent exploding.
+# 을 이용합니다.
 #
 
 criterion = nn.CrossEntropyLoss()
-lr = 5.0 # learning rate
+lr = 5.0 # 학습률
 optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
 
 import time
 def train():
-    model.train() # Turn on the train mode
+    model.train() # 학습 모드를 시작합니다.
     total_loss = 0.
     start_time = time.time()
     ntokens = len(TEXT.vocab.stoi)
@@ -277,7 +267,7 @@ def train():
             start_time = time.time()
 
 def evaluate(eval_model, data_source):
-    eval_model.eval() # Turn on the evaluation mode
+    eval_model.eval() # 평가 모드를 시작합니다.
     total_loss = 0.
     ntokens = len(TEXT.vocab.stoi)
     with torch.no_grad():
@@ -289,11 +279,11 @@ def evaluate(eval_model, data_source):
     return total_loss / (len(data_source) - 1)
 
 ######################################################################
-# Loop over epochs. Save the model if the validation loss is the best
-# we've seen so far. Adjust the learning rate after each epoch.
+# 에포크 내에서 반복됩니다. 만약 검증 오차 (validation loss) 가 우리가 지금까지 관찰한 것 중 최고라면 모델을 저장합니다.
+# 매 에포크 이후에 학습률을 조절합니다.
 
 best_val_loss = float("inf")
-epochs = 3 # The number of epochs
+epochs = 3 # 에포크 수
 best_model = None
 
 for epoch in range(1, epochs + 1):
@@ -314,10 +304,10 @@ for epoch in range(1, epochs + 1):
 
 
 ######################################################################
-# Evaluate the model with the test dataset
+# 평가 데이터셋 (test dataset) 으로 모델을 평가하기
 # -------------------------------------
 #
-# Apply the best model to check the result with the test dataset.
+# 평가 데이터셋에 대한 결과를 확안하기 위해서 최고의 모델을 적용합니다.
 
 test_loss = evaluate(best_model, test_data)
 print('=' * 89)
