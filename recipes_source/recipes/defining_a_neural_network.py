1deafe8df3196a4d412d07de583f02a7c085c03c
potatochips178 <potatochips@sookmyung.ac.kr>
2020-08-08 05:29:30 +0900

    recipes/defining_a_neural_network.py 번역 (#88)

--- recipes_source/recipes/defining_a_neural_network.py
+++ recipes_source/recipes/defining_a_neural_network.py
@@ -1,28 +1,20 @@
 """
-Defining a Neural Network in PyTorch
+Pytorch를 사용해 신경망 정의하기
 ====================================
-Deep learning uses artificial neural networks (models), which are
-computing systems that are composed of many layers of interconnected
-units. By passing data through these interconnected units, a neural
-network is able to learn how to approximate the computations required to
-transform inputs into outputs. In PyTorch, neural networks can be
-constructed using the ``torch.nn`` package.
+딥러닝은 인공신경망(models)을 사용하며 이것은 상호연결된 집단의 많은 계층으로 구성된 계산 시스템입니다. 
+데이터가 이 상호연결된 집단을 통과하면서, 신경망은 입력을 출력으로 바꾸기 위해 요구된 계산 방법에 어떻게 근접하는 지를 배울 수 있습니다.  
+PyTorch에서, 신경망은 ``torch.nn`` 패키지를 사용해 구성할 수 있습니다. 
 
-Introduction
-------------
-PyTorch provides the elegantly designed modules and classes, including
-``torch.nn``, to help you create and train neural networks. An
-``nn.Module`` contains layers, and a method ``forward(input)`` that
-returns the ``output``.
-
-In this recipe, we will use ``torch.nn`` to define a neural network
-intended for the `MNIST
-dataset <https://pytorch.org/docs/stable/torchvision/datasets.html#mnist>`__.
-
-Setup
+소개
 -----
-Before we begin, we need to install ``torch`` if it isn’t already
-available.
+PyTorch는 ``torch.nn`` 을 포함하여 신경망을 만들고 훈련시키는 것을 도울 수 있도록 섬세하게 만들어진 모듈과 클래스들을 제공합니다. 
+``nn.Moduel`` 은 계층, 그리고 ``output`` 을 반환하는 ``forward(input)`` 메소드를 포함하고 있습니다. 
+
+이 레시피에서, `MNIST dataset <https://pytorch.org/docs/stable/torchvision/datasets.html#mnist>`__ 을 사용하여 신경망을 정의하기 위해 ``torch.nn`` 을 사용할 예정입니다.
+
+설치
+-----
+시작하기 전에, 준비가 되어있지 않다면 ``torch`` 를 설치해야 합니다. 
 
 ::
 
@@ -33,19 +25,18 @@ available.
 
 
 ######################################################################
-# Steps
+# 단계
 # -----
 # 
-# 1. Import all necessary libraries for loading our data
-# 2. Define and intialize the neural network
-# 3. Specify how data will pass through your model
-# 4. [Optional] Pass data through your model to test
+# 1. 데이터를 가져오기 위해 필요한 라이브러리들 불러오기
+# 2. 신경망을 정의하고 초기화하기
+# 3. 데이터가 모델을 어떻게 지나갈 지 구체화하기
+# 4. [선택사항] 시험해보기 위해 데이터가 모델을 지나가게 하기
 # 
-# 1. Import necessary libraries for loading our data
+# 1. 데이터를 가져오기 위해 필요한 라이브러리들 불러오기
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 
-# For this recipe, we will use ``torch`` and its subsidiaries ``torch.nn``
-# and ``torch.nn.functional``.
+# 이 레시피에서, ``torch`` 과 이것의 하위 모듈인 ``torch.nn`` , ``torch.nn.functional`` 을 사용합니다.   
 # 
 
 import torch
@@ -54,45 +45,40 @@ import torch.nn.functional as F
 
 
 ######################################################################
-# 2. Define and intialize the neural network
+# 2. 신경망을 정의하고 초기화하기
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# 이미지를 인식하는 신경망을 만들겁니다. PyTorch에서 만들어진 컨볼루션(convolution)이라고 불리는 방법을 사용하겠습니다.
+# 컨볼루션은 커널이나 작은 메트릭스를 통해 가중치를 부여한 이미지의 각 요소를 주변 값과 더합니다. 
+# 그리고 이것은 입력된 이미지의 특징(모서리 감지, 선명함, 흐릿함 등과 같은)을 추출하는 데 도움을 줍니다. 
+#
+# 모델의 ``Net`` 클래스를 정의하기 위해 2가지가 필요합니다. 
+# 첫번째는 ``nn.Module`` 을 참고하는 ``__init__`` 함수를 작성하는 것입니다. 
+# 이 함수는 신경망에서  fully connected layers를 만드는 것에 사용됩니다.
 # 
-# Our network will recognize images. We will use a process built into
-# PyTorch called convolution. Convolution adds each element of an image to
-# its local neighbors, weighted by a kernel, or a small martrix, that
-# helps us extract certain features (like edge detection, sharpness,
-# blurriness, etc.) from the input image.
-# 
-# There are two requirements for defining the ``Net`` class of your model.
-# The first is writing an ``__init__`` function that references
-# ``nn.Module``. This function is where you define the fully connected
-# layers in your neural network.
-# 
-# Using convolution, we will define our model to take 1 input image
-# channel, and output match our target of 10 labels representing numbers 0
-# through 9. This algorithm is yours to create, we will follow a standard
-# MNIST algorithm.
-# 
+# 컨볼루션을 사용해, 1개의 입력 이미지 채널을 가지고 
+# 목표인 0부터 9까지 숫자를 대표하는 10개의 라벨과 되응되 값을 출력하는 모델을 정의하겠습니다.
+# 이 알고리즘은 만드는 사람에 달렸지만, 기본적인 MNIST 알고리즘을 따르도록 하겠습니다. 
+#  
 
 class Net(nn.Module):
     def __init__(self):
       super(Net, self).__init__()
 
-      # First 2D convolutional layer, taking in 1 input channel (image),
-      # outputting 32 convolutional features, with a square kernel size of 3
+      # 첫번째 2D 컨볼루션 계층
+      # 1개의 입력 채널(이미지)을 받아들이고, 사각 커널 사이즈가 3인 32개의 컨볼루션 특징들을 출력합니다. 
       self.conv1 = nn.Conv2d(1, 32, 3, 1)
-      # Second 2D convolutional layer, taking in the 32 input layers,
-      # outputting 64 convolutional features, with a square kernel size of 3
+      # 두번째 2D 컨볼루션 계층
+      # 32개의 입력 게층을 받아들이고, 사각 커널 사이즈가 3인 64개의 컨볼루션 특징을 출력합니다. 
       self.conv2 = nn.Conv2d(32, 64, 3, 1)
 
-      # Designed to ensure that adjacent pixels are either all 0s or all active
-      # with an input probability
+      # 인접한 픽셀들은 입력 확률에 따라 모두 0 값을 가지거나 혹은 모두 유효한 값이 되도록 만듭니다.
       self.dropout1 = nn.Dropout2d(0.25)
       self.dropout2 = nn.Dropout2d(0.5)
 
-      # First fully connected layer
+      # 첫번째 fully connected layer
       self.fc1 = nn.Linear(9216, 128)
-      # Second fully connected layer that outputs our 10 labels
+      # 10개의 라벨을 출력하는 두번째 fully connected layer
       self.fc2 = nn.Linear(128, 10)
 
 my_nn = Net()
@@ -100,18 +86,15 @@ print(my_nn)
 
 
 ######################################################################
-# We have finished defining our neural network, now we have to define how
-# our data will pass through it.
+# 신경망을 정의하는 것을 마쳤습니다. 이제 어떻게 이것을 지나갈 지 정의해야 합니다. 
 # 
-# 3. Specify how data will pass through your model
+# 3. 데이터가 모델을 어떻게 지나갈 지 구체화하기
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# PyTorch를 사용해 모델을 생성할 때, 계산 그래프(즉, 신경망)에 데이터를 지나가게 하는 ``forward`` 함수를 정의해야 합니다. 
+# 이것은 feed-forward 알고리즘을 나타냅니다.
 # 
-# When you use PyTorch to build a model, you just have to define the
-# ``forward`` function, that will pass the data into the computation graph
-# (i.e. our neural network). This will represent our feed-forward
-# algorithm.
-# 
-# You can use any of the Tensor operations in the ``forward`` function.
+# ``forward`` 함수에서 어떠한 Tensor 연산자도 사용 가능합니다. 
 # 
 
 class Net(nn.Module):
@@ -124,42 +107,41 @@ class Net(nn.Module):
       self.fc1 = nn.Linear(9216, 128)
       self.fc2 = nn.Linear(128, 10)
 
-    # x represents our data
+    # x는 데이터를 대표합니다. 
     def forward(self, x):
-      # Pass data through conv1
+      # conv1을 데이터가 지나갑니다. 
       x = self.conv1(x)
-      # Use the rectified-linear activation function over x
+      # x를 ReLU 활성함수(rectified-linear activation function)에 대입합니다. 
       x = F.relu(x)
 
       x = self.conv2(x)
       x = F.relu(x)
 
-      # Run max pooling over x
+      # x에 대해서 max pooling을 실행합니다. 
       x = F.max_pool2d(x, 2)
-      # Pass data through dropout1
+      # 데이터가 dropout1을 지나갑니다. 
       x = self.dropout1(x)
-      # Flatten x with start_dim=1
+      # start_dim=1으로 x를 압축합니다. 
       x = torch.flatten(x, 1)
-      # Pass data through fc1
+      # 데이터를 fc1을 지나갑니다. 
       x = self.fc1(x)
       x = F.relu(x)
       x = self.dropout2(x)
       x = self.fc2(x)
 
-      # Apply softmax to x 
+      # x에 softmax를 적용합니다. 
       output = F.log_softmax(x, dim=1)
       return output
 
 
 ######################################################################
-# 4. [Optional] Pass data through your model to test
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 4. [선택사항] 시험해보기 위해 데이터가 모델을 지나가게 하기
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 
-# To ensure we receive our desired output, let’s test our model by passing
-# some random data through it.
+# 원하는 출력값을 받을 수 있는 지 확인하기 위해, 무작위의 데이터를 모델에 통과시켜 시험해봅시다. 
 # 
 
-# Equates to one random 28x28 image
+# 임의의 28x28 이미지로 맞춰줍니다. 
 random_data = torch.rand((1, 1, 28, 28))
 
 my_nn = Net()
@@ -168,16 +150,14 @@ print (result)
 
 
 ######################################################################
-# Each number in this resulting tensor equates to the prediction of the
-# label the random tensor is associated to.
+# 결과 tensor의 각 숫자는 임의의 tenosr와 연관된 라벨이 예측한 값과 같다는 것을 나타냅니다.   
 # 
-# Congratulations! You have successfully defined a neural network in
-# PyTorch.
+# 축하합니다! PyTorch로 신경망 정의하기를 성공적으로 해냈습니다. 
 # 
-# Learn More
-# ----------
+# 더 알아보기
+# -----------
 # 
-# Take a look at these other recipes to continue your learning:
+# 게속해서 학습하고 싶다면 다른 레시피를 살펴보싶시오:
 # 
-# - `What is a state_dict in PyTorch <https://pytorch.org/tutorials/recipes/recipes/what_is_state_dict.html>`__
-# - `Saving and loading models for inference in PyTorch <https://pytorch.org/tutorials/recipes/recipes/saving_and_loading_models_for_inference.html>`__
+# - `PyTorch에서 state_dict이 무엇인지 <https://pytorch.org/tutorials/recipes/recipes/what_is_state_dict.html>`__
+# - `PyTorch로 추론을 위한 모델을 저장하고 가저오기  <https://pytorch.org/tutorials/recipes/recipes/saving_and_loading_models_for_inference.html>`__
