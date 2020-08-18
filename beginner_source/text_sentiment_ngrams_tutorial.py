384d803327f69395153119dd5434ee85dfcbd9f8
Gangmin-Kim <42054789+Gangsss@users.noreply.github.com>
2020-08-18 12:46:13 +0900

    Update beginner_source/text_sentiment_ngrams_tutorial.py
    Co-authored-by: SINYU JEONG  <shsy0404g@gmail.com>

--- beginner_source/text_sentiment_ngrams_tutorial.py
+++ beginner_source/text_sentiment_ngrams_tutorial.py
@@ -130,7 +130,7 @@ model = TextSentiment(VOCAB_SIZE, EMBED_DIM, NUN_CLASS).to(device)
 # 이는 함수가 각 worker마다 이용가능하게 하는 것을 보장해줍니다.
 #
 # 원본 데이터 배치 인풋 안의 텍스트들은 리스트에 들어가고 단일 텐서로서 ``nn.EmbeddingBag`` 의 입력으로 연결합니다.
-# offsets은 텍스트 텐서에서 개별 시퀀스의 시작 인덱스를 표현하는 구분자들의 텐서이다.
+# offsets은 텍스트 텐서에서 개별 시퀀스의 시작 인덱스를 표현하는 구분자들의 텐서입니다.
 # label은 각 텍스트 항목별 라벨을 저장해 두는 tensor이다.
 #
 
