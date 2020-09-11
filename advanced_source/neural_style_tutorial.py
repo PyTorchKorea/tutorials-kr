779f9355f8eeee41cb229263178b81d91bd4568f
jjeamin <39845657+jjeamin@users.noreply.github.com>
2020-09-11 14:11:45 +0900

    Update advanced_source/neural_style_tutorial.py
    Co-authored-by: ajin-jng <68931515+ajin-jng@users.noreply.github.com>

--- advanced_source/neural_style_tutorial.py
+++ advanced_source/neural_style_tutorial.py
@@ -247,7 +247,7 @@ cnn = models.vgg19(pretrained=True).features.to(device).eval()
 # 추가적으로, VGG 네트워크는
 # mean=[0.485, 0.456, 0.406] 와 std=[0.229, 0.224, 0.225]로 각 채널이 정규화 된
 # 이미지로 훈련됩니다.
-# 이미지를 네트워크로 입력하기 전에 정규화 하는데 사용합니다.
+# 이미지를 네트워크로 입력하기 전에 정규화하는데 사용합니다.
 #
 
 cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
