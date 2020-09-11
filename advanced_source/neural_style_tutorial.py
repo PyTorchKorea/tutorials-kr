f1b8eefb03c1448dab34a8e41185f1a6410dc127
jjeamin <39845657+jjeamin@users.noreply.github.com>
2020-09-11 14:07:15 +0900

    Update advanced_source/neural_style_tutorial.py
    Co-authored-by: ajin-jng <68931515+ajin-jng@users.noreply.github.com>

--- advanced_source/neural_style_tutorial.py
+++ advanced_source/neural_style_tutorial.py
@@ -161,7 +161,7 @@ class ContentLoss(nn.Module):
 
     def __init__(self, target,):
         super(ContentLoss, self).__init__()
-        # 기울기를 동적으로 계산하는데 사용되는 tree로부터 타겟 Content를 `분리(detach)` 합니다.
+        # 기울기를 동적으로 계산하는데 사용되는 tree로부터 타깃 Content를 `분리(detach)` 합니다.
         # 이것은 변수가 아니라 명시된 값입니다.
         # 그렇지 않으면 criterion의 forward 메소드에서 오류가 발생합니다.
         self.target = target.detach()
