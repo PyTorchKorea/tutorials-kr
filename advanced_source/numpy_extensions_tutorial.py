a928526d010ba4a6d96b661cc926160c9c26da72
GitHub Action <reserve.dev@gmail.com>
2021-04-11 20:55:43 +0900

    advanced/numpy_extensions_tutorial.py 번역 개선 (#141, #157)

--- advanced_source/numpy_extensions_tutorial.py
+++ advanced_source/numpy_extensions_tutorial.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-numpy 와 scipy 를 이용한 확장(Extensions) 생성하기
+numpy 와 scipy 를 이용한 확장(Extensions) 만들기
 =====================================================
 **Author**: `Adam Paszke <https://github.com/apaszke>`_
 
@@ -10,27 +10,25 @@ numpy 와 scipy 를 이용한 확장(Extensions) 생성하기
 
 이번 튜토리얼에서는 두 가지 작업을 수행할 것입니다:
 
-1. 매개 변수가 없는 신경망 계층 만들기
+1. 매개 변수가 없는 신경망 계층(layer) 만들기
+    - 이는 구현의 일부로 **numpy** 를 호출합니다.
 
-    - 이는 구현의 일부로서 **numpy** 를 호출합니다.
-
-2. 학습 가능한 가중치가 있는 신경망 계층 생성하기
-
-    - 이는 구현의 일부로서 **Scipy** 를 호출합니다.
+2. 학습 가능한 가중치가 있는 신경망 계층(layer) 만들기
+    - 이는 구현의 일부로 **Scipy** 를 호출합니다.
 """
 
 import torch
 from torch.autograd import Function
 
 ###############################################################
-# 매개 변수가 없는 예
-# ----------------------
+# 매개 변수가 없는(Parameter-less) 예시
+# ----------------------------------------
 #
-# 이 레이어는 특별히 유용하거나 수학적으로 올바른 작업을 수행하지 않습니다.
+# 이 계층(layer)은 특별히 유용하거나 수학적으로 올바른 작업을 수행하지 않습니다.
 #
-# 이름은 BadFFTFunction입니다.
+# 이름은 대충 BadFFTFunction으로 지었습니다.
 #
-# **레이어 구현**
+# **계층(layer) 구현**
 
 from numpy.fft import rfft2, irfft2
 
@@ -48,14 +46,14 @@ class BadFFTFunction(Function):
         result = irfft2(numpy_go)
         return grad_output.new(result)
 
-# 이 계층에는 매개 변수가 없으므로 nn.Module 클래스가 아닌 함수로 간단히 선언 할 수 있습니다.
+# 이 계층에는 매개 변수가 없으므로 nn.Module 클래스가 아닌 함수로 간단히 선언할 수 있습니다.
 
 
 def incorrect_fft(input):
     return BadFFTFunction.apply(input)
 
 ###############################################################
-# **생성된 레이어의 사용 예시:**
+# **생성된 계층(layer)의 사용 예시:**
 
 input = torch.randn(8, 8, requires_grad=True)
 result = incorrect_fft(input)
@@ -64,15 +62,16 @@ result.backward(torch.randn(result.size()))
 print(input)
 
 ###############################################################
-# 매개 변수화 된 예시
-# --------------------
+# 매개 변수가 있는(Parameterized) 예시
+# ----------------------------------------
 #
-# 딥러닝 문헌에서 이 레이어는 혼동스럽게도 컨볼루션이라고 칭하지만 실제로는 상호 상관 연산입니다.
-# (컨볼루션과 상호 상관의 유일한 차이점은 컨볼루션에서는 필터를 뒤집어서 연산을 한다는 것입니다).
+# 딥러닝 문헌에서 이 계층(layer)의 실제 연산은 상호 상관(cross-correlation)이지만
+# 합성곱(convolution)이라고 헷갈리게 부르고 있습니다.
+# (합성곱은 필터를 뒤집어서 연산을 하는 반면, 상호 상관은 그렇지 않은 차이가 있습니다)
 #
-# 학습 가능한 가중치를 가진 필터 (커널)을 갖는 교차 상관 연산 계층의 구현
+# 학습 가능한 가중치를 가는 필터(커널)를 갖는 상호 상관 계층을 구현해보겠습니다.
 #
-# 역방향 패스는 입력에 대한 기울기와 필터에 대한 기울기를 계산합니다.
+# 역전파 단계(backward pass)에서는 입력에 대한 기울기(gradient)와 필터에 대한 기울기를 계산합니다.
 
 from numpy import flip
 import numpy as np
@@ -84,7 +83,7 @@ from torch.nn.parameter import Parameter
 class ScipyConv2dFunction(Function):
     @staticmethod
     def forward(ctx, input, filter, bias):
-        # detach so we can cast to NumPy
+        # 분리(detach)하여 NumPy로 변환(cast)할 수 있습니다.
         input, filter, bias = input.detach(), filter.detach(), bias.detach()
         result = correlate2d(input.numpy(), filter.numpy(), mode='valid')
         result += bias.numpy()
@@ -98,7 +97,7 @@ class ScipyConv2dFunction(Function):
         grad_output = grad_output.numpy()
         grad_bias = np.sum(grad_output, keepdims=True)
         grad_input = convolve2d(grad_output, filter.numpy(), mode='full')
-        # the previous line can be expressed equivalently as:
+        # 윗줄은 다음과 같이 표현할 수도 있습니다:
         # grad_input = correlate2d(grad_output, flip(flip(filter.numpy(), axis=0), axis=1), mode='full')
         grad_filter = correlate2d(input.numpy(), grad_output, mode='valid')
         return torch.from_numpy(grad_input), torch.from_numpy(grad_filter).to(torch.float), torch.from_numpy(grad_bias).to(torch.float)
@@ -126,7 +125,7 @@ output.backward(torch.randn(8, 8))
 print("Gradient for the input map: ", input.grad)
 
 ###############################################################
-# **기울기 확인:**
+# **기울기(gradient) 확인:**
 
 from torch.autograd.gradcheck import gradcheck
 
