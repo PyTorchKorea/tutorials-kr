7dd82d2b3fbefc321b3b729aebb95682b717efca
hihunjin <hihunjin@hanmail.net>
2021-05-17 18:31:49 +0900

    Add more translation

--- recipes_source/recipes/amp_recipe.py
+++ recipes_source/recipes/amp_recipe.py
@@ -16,7 +16,7 @@
 
 이 레시피는 먼저 기본 정밀도에서 간단한 신경망의 성능을 측정하고, 다음으로 ``autocast`` 와 ``GradScaler`` 를 추가하여, 같은 신경망에서 향상된 성능을 살펴봅니다.
 
-이 레시피를 파이썬 스크립트로 다운로드하고 실행해 볼 수 있습니다. 환경은 Pytorch 1.6+와 CUDA-capable GPU가 필요합니다.
+이 레시피를 파이썬 스크립트로 다운로드하고 실행해 볼 수 있습니다. 환경은 Pytorch 1.6+와 쿠다를 지원하는 GPU가 필요합니다.
 
 혼합 정밀도는 원시적으로 텐서 코어를 사용할 수 있는 아키텍처(볼타, 튜링, 암페어)에서 효과를 잘 발휘합니다. 
 이 레시피는 이 아키텍처에서 2에서 3배의 속도 향상을 보여줍니다. 이전 버전의 아키텍처(케플러, 맥스웰, 파스칼)에서는, 완만한 속도 향상을 관찰할 수 있습니다. 
@@ -46,7 +46,7 @@ def end_timer_and_print(local_msg):
 ##########################################################
 # 간단한 신경망
 # ------------------------
-# 아래 선형 계층과 ReLU의 연속으로 이루어진 모델이 혼합 정밀도로 속도 향상이 될 것입니다.
+# 아래 선형 계층과 ReLU 활성함수(rectified-linear activation function)의 연속으로 이루어진 모델이 혼합 정밀도로 속도 향상이 될 것입니다.
 
 def make_model(in_size, out_size, num_layers):
     layers = []
@@ -81,7 +81,7 @@ loss_fn = torch.nn.MSELoss().cuda()
 
 ##########################################################
 # 기본 정밀도
-# -----------------
+# ----------------------------------
 # ``torch.cuda.amp`` 없이, 기본 정밀도(``torch.float32``)로 아래 간단한 신경망를 실행합니다.
 
 net = make_model(in_size, out_size, num_layers)
@@ -99,7 +99,7 @@ end_timer_and_print("Default precision:")
 
 ##########################################################
 # autocast 사용하기
-# ---------------
+# --------------------------------
 # `torch.cuda.amp.autocast <https://pytorch.org/docs/stable/amp.html#torch.cuda.amp.autocast>`_ 인스턴스는 컨텍스트 매니저로 제공되어, 혼합 정밀도로 실행되는 코드 영역(region)을 제공합니다
 # 
 # 이 영역에서는, 쿠다 연산이 autocast에 의해 선택된 데이터 타입으로 실행됩니다. 이는 정확도를 유지하면서 성능을 향상시킵니다. 
@@ -126,10 +126,10 @@ for epoch in range(0): # 0 epochs, 이 섹션은 설명을 위한 스크립트�
 
 ##########################################################
 # GradScaler 사용하기
-# -----------------
-# `Gradient scaling <https://pytorch.org/docs/stable/amp.html#gradient-scaling>`_ 은 혼합 정밀도 훈련 시 작은 크기의 gradient 값이 0으로 바뀌는 underflowing 현상을 막습니다.
+# ----------------------------------
+# `Gradient scaling <https://pytorch.org/docs/stable/amp.html#gradient-scaling>`_ 은 혼합 정밀도 훈련 시 작은 크기의 그래디언트 값이 0으로 바뀌는 underflowing 현상을 막습니다.
 # 
-# `torch.cuda.amp.GradScaler <https://pytorch.org/docs/stable/amp.html#torch.cuda.amp.GradScaler>`_ 는 gradient를 크기를 편리하게 조절합니다.
+# `torch.cuda.amp.GradScaler <https://pytorch.org/docs/stable/amp.html#torch.cuda.amp.GradScaler>`_ 는 그래디언트를 크기를 편리하게 조절합니다.
 
 # scaler를 먼저 생성합니다.
 # 신경망가 수렴하지 않는다면 pytorch github에 issue를 생성해주세요.
@@ -143,22 +143,22 @@ for epoch in range(0): # 0 epochs, 이 섹션은 설명을 위한 스크립트�
             output = net(input)
             loss = loss_fn(output, target)
 
-        # Scales 손실값. 스케일된 gradient를 만들기 위해, backward()를 스케일된 손실값에 적용해주세요.
+        # 스케일된 손실값. 스케일된 그래디언트를 만들기 위해, backward()를 스케일된 손실값에 적용해주세요.
         scaler.scale(loss).backward()
 
-        # sacler.step() 은 먼저 optimizer에 할당된 파라미터들을 unscale합니다.
-        # 이 gradients들이 inf나 NaN을 가지고 있지 않다면, optimizer.step()이 호출됩니다.
+        # sacler.step() 은 먼저 옵티마이저에 할당된 파라미터들을 unscale합니다.
+        # 이 그래디언트들이 inf나 NaN을 가지고 있지 않다면, optimizer.step()이 호출됩니다.
         # 가지고 있다면, optimizer.step()은 건너뜁니다.
         scaler.step(opt)
 
-        # 다음 루프를 위해 scale을 업데이트 합니다.
+        # 다음 루프를 위해 스케일을 업데이트 합니다.
         scaler.update()
 
         opt.zero_grad()
 
 ##########################################################
 # 모두 합치기 : "자동 혼합 정밀도"
-# ------------------------------------------
+# -----------------------------------------------------------
 # ``use_amp`` 를 통해 ``autocast`` 와 ``GradScaler`` 를 끄고 켤 수 있습니다.
 
 use_amp = True
@@ -180,9 +180,9 @@ for epoch in range(epochs):
 end_timer_and_print("Mixed precision:")
 
 ##########################################################
-# Gradient를 검사하고 수정하기(gradient 클리핑)
-# --------------------------------------------------------
-# ``scaler.scale(loss).backward()`` 로 생성된 모든 Gradient는 스케일링됩니다. 
+# 그래디언트를 검사하고 수정하기(그래디언트 클리핑)
+# -------------------------------------------------------------------------
+# ``scaler.scale(loss).backward()`` 로 생성된 모든 그래디언트는 스케일링됩니다. 
 # ``backward()`` 와 ``scaler.step(optimizer)`` 사이에서 파라미터의 ``.grad`` 어트리뷰트로 검사하고 싶다면, 
 # `scaler.unscale_(optimizer) <https://pytorch.org/docs/stable/amp.html#torch.cuda.amp.GradScaler.unscale_>`_ 를 사용해 먼저 스케일링을 해제해야 합니다.
 
@@ -193,10 +193,10 @@ for epoch in range(0): # 0 epochs, 이 섹션은 설명을 위한 스크립트�
             loss = loss_fn(output, target)
         scaler.scale(loss).backward()
 
-        # Gradient를 unscale합니다.
+        # 그래디언트의 스케일을 원래 스케일로 되돌립니다.
         scaler.unscale_(opt)
 
-        # optimizer에 할당된 파라미터들의 gradient가 unscaled되었기 때문에, 평소처럼 클리핑을 합니다.
+        # 옵티마이저에 할당된 파라미터들의 그래디언트가 원래의 스케일로 돌아왔기 때문에, 평소처럼 클리핑을 합니다.
         # max_norm을 같은 값으로 사용할 수 있습니다.
         torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=0.1)
 
@@ -206,10 +206,10 @@ for epoch in range(0): # 0 epochs, 이 섹션은 설명을 위한 스크립트�
 
 ##########################################################
 # 저장하기/다시 시작하기
-# ----------------
+# --------------------------------------------------
 # AMP를 사용한 저장/재시작은 `scaler.state_dict <https://pytorch.org/docs/stable/amp.html#torch.cuda.amp.GradScaler.state_dict>`_ 와 `scaler.load_state_dict <https://pytorch.org/docs/stable/amp.html#torch.cuda.amp.GradScaler.load_state_dict>`_ 를 사용합니다.
 # 
-# 저장을 할 때는, scaler의 state dict를 모델과 optimizer의 state dict과 함께 저장합니다. 
+# 저장을 할 때는, scaler의 state dict를 모델과 옵티마이저의 state dict과 함께 저장합니다. 
 # 이것을 루프 시작 전에 하거나, ``scaler.update()`` 후 루프의 끝에 합니다.
 
 checkpoint = {"model": net.state_dict(),
@@ -218,7 +218,7 @@ checkpoint = {"model": net.state_dict(),
 # torch.save(checkpoint, "filename")
 
 ##########################################################
-# 재시작 할 때는, scaler의 state dict을 모델과 optimizer와 함께 가져옵니다.
+# 재시작 할 때는, scaler의 state dict을 모델과 옵티마이저와 함께 가져옵니다.
 
 # checkpoint = torch.load("filename",
 #                         map_location = lambda storage, loc: storage.cuda(dev))
@@ -233,7 +233,7 @@ scaler.load_state_dict(checkpoint["scaler"])
 
 ##########################################################
 # 추론/평가
-# --------------------
+# -------------------------------------
 # ``autocast`` 가 추론과 평가의 순전파를 감쌀 수 있습니다. ``GradScaler`` 는 필요하지 않습니다.
 
 ##########################################################
