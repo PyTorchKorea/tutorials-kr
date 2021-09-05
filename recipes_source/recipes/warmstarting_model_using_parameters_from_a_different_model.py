9948089a6cf9f6d3a70f9459a7955203266935d2
Jinhyun Kim <lewha0@gmail.com>
2021-09-05 17:35:09 +0900

    기존 내용 반영 후 웜스타트 용어 수정 및 파라미터를 매개변수로 수정

--- recipes_source/recipes/warmstarting_model_using_parameters_from_a_different_model.py
+++ recipes_source/recipes/warmstarting_model_using_parameters_from_a_different_model.py
@@ -1,25 +1,24 @@
 """
-Warmstarting model using parameters from a different model in PyTorch
-=====================================================================
-Partially loading a model or loading a partial model are common
-scenarios when transfer learning or training a new complex model.
-Leveraging trained parameters, even if only a few are usable, will help
-to warmstart the training process and hopefully help your model converge
-much faster than training from scratch.
+PyTorch에서 다른 모델의 매개변수를 사용하여 빠르게 모델 시작하기(warmstart)
+===========================================================================
+기존 모델을 부분적으로 불러오거나, 혹은 부분적인 모델을 불러오는
+것은 학습 전이(Transfer learning)나 복잡한 모델을 새로 학습할 때 자주
+접하게 되는 시나리오입니다. 학습된 매개변수를 활용하면 학습 과정을
+빠르게 시작(warmstart)할 수 있으며, 그러면 여러분의 모델을 처음부터
+훈련시킬 때보다 훨씬 일찍 수렴하리라 기대할 수 있습니다. 이는 활용할
+수 있는 매개변수가 얼마 안 될 때에도 마찬가지입니다.
 
-Introduction
-------------
-Whether you are loading from a partial ``state_dict``, which is missing
-some keys, or loading a ``state_dict`` with more keys than the model
-that you are loading into, you can set the strict argument to ``False``
-in the ``load_state_dict()`` function to ignore non-matching keys.
-In this recipe, we will experiment with warmstarting a model using
-parameters of a different model.
+도입
+----
+여러분이 일부 키가 누락된 부분적인 ``state_dict`` 를 불러올 때든, 아니면
+결과를 저장할 모델보다 키가 많은 ``state_dict`` 를 불러올 때든,
+``load_state_dict()`` 함수의 인자인 strict 를 False 로 두면 매치되지
+않는 키를 무시하게끔 할 수 있습니다. 이 레시피에서는 다른 모델의
+매개변수를 사용하여 모델을 빠르게 시작하는 실험을 진행해 보려 합니다.
 
-Setup
------
-Before we begin, we need to install ``torch`` if it isn’t already
-available.
+설정
+----
+시작에 앞서서, ``torch`` 가 준비되어 있지 않다면 설치해야 합니다.
 
 ::
 
@@ -30,19 +29,19 @@ available.
 
 
 ######################################################################
-# Steps
-# -----
+# 단계
+# ----
 # 
-# 1. Import all necessary libraries for loading our data
-# 2. Define and intialize the neural network A and B
-# 3. Save model A
-# 4. Load into model B
+# 1. 데이터를 불러오는데 필요한 모든 라이브러리를 import 합니다
+# 2. 신경망 A와 B를 정의하고 초기화합니다
+# 3. 모델 A를 저장합니다
+# 4. 모델 B로 모델을 불러옵니다
 # 
-# 1. Import necessary libraries for loading our data
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 1. 데이터를 불러올 때 필요한 라이브러리 import 하기
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 
-# For this recipe, we will use ``torch`` and its subsidiaries ``torch.nn``
-# and ``torch.optim``.
+# 이 레시피에서는 ``torch`` 와, 그 하위 패키지인 ``torch.nn`` 및
+# ``torch.optim`` 을 사용하겠습니다.
 # 
 
 import torch
@@ -51,13 +50,13 @@ import torch.optim as optim
 
 
 ######################################################################
-# 2. Define and intialize the neural network A and B
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 2. 신경망 A와 B 정의하고 초기화하기
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 
-# For sake of example, we will create a neural network for training
-# images. To learn more see the Defining a Neural Network recipe. We will
-# create two neural networks for sake of loading one parameter of type A
-# into type B.
+# 우리는 하나의 예로써 이미지를 학습하는 신경망을 만들어 보려 합니다.
+# 이에 대해 좀 더 알아보고 싶다면 신경망 정의하기에 대한 레시피를
+# 참고하시기 바랍니다. 우리는 신경망을 두 개 만들려고 하는데, 신경망 A의
+# 매개변수를 신경망 B로 불러오려 합니다.
 # 
 
 class NetA(nn.Module):
@@ -104,39 +103,39 @@ netB = NetB()
 
 
 ######################################################################
-# 3. Save model A
-# ~~~~~~~~~~~~~~~~~~~
+# 3. 모델 A 저장하기
+# ~~~~~~~~~~~~~~~~~~
 # 
 
-# Specify a path to save to
+# 모델을 저장할 경로를 지정해 줍니다
 PATH = "model.pt"
 
 torch.save(netA.state_dict(), PATH)
 
 
 ######################################################################
-# 4. Load into model B
-# ~~~~~~~~~~~~~~~~~~~~~~~~
+# 4. 모델 B로 불러오기
+# ~~~~~~~~~~~~~~~~~~~~
 # 
-# If you want to load parameters from one layer to another, but some keys
-# do not match, simply change the name of the parameter keys in the
-# state_dict that you are loading to match the keys in the model that you
-# are loading into.
+# 한 레이어의 매개변수를 다른 레이어로 불러오려 하는데 일부 키가 매치되지
+# 않는 상황이라고 해 봅시다. 그럴 때는 여러분이 불러오려 하는 state_dict
+# 의 매개변수 키의 이름을 바꿔서, 불러온 모델을 저장하려는 모델의 키와
+# 매치되도록 해 주면 됩니다.
 # 
 
 netB.load_state_dict(torch.load(PATH), strict=False)
 
 
 ######################################################################
-# You can see that all keys matched successfully!
+# 모든 키가 성공적으로 매치되었음을 확인할 수 있을 것입니다!
 # 
-# Congratulations! You have successfully warmstarted a model using
-# parameters from a different model in PyTorch.
+# 축하합니다! 여러분은 PyTorch에서 다른 모델의 매개변수를 사용하여
+# 모델을 빠르게 시작하는 방법에 대해 살펴보았습니다.
 # 
-# Learn More
-# ----------
+# 좀 더 알아보기
+# --------------
 # 
-# Take a look at these other recipes to continue your learning:
+# 계속 공부해 나가면서 다음 두 레시피를 살펴보기를 권합니다.
 # 
-# - `Saving and loading multiple models in one file using PyTorch <https://tutorials.pytorch.kr/recipes/recipes/saving_multiple_models_in_one_file.html>`__
-# - `Saving and loading models across devices in PyTorch <https://tutorials.pytorch.kr/recipes/recipes/save_load_across_devices.html>`__
+# - `PyTorch에서 여러 모델을 하나의 파일에 저장하기 & 불러오기 <https://tutorials.pytorch.kr/recipes/recipes/saving_multiple_models_in_one_file.html>`__
+# - `PyTorch에서 다양한 장치 간 모델을 저장하고 불러오기 <https://tutorials.pytorch.kr/recipes/recipes/save_load_across_devices.html>`__
