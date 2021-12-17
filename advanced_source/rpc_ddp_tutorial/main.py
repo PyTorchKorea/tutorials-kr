bd4ca151f03364ce43688089e89f351944d913d2
dajeongPark-dev <jul897813@gmail.com>
2021-12-18 02:09:17 +0900

    Update main.py
    Incomplete _considering about how to solve the 'optimizer'

--- advanced_source/rpc_ddp_tutorial/main.py
+++ advanced_source/rpc_ddp_tutorial/main.py
@@ -18,10 +18,10 @@ EMBEDDING_DIM = 16
 # BEGIN hybrid_model
 class HybridModel(torch.nn.Module):
     r"""
-    The model consists of a sparse part and a dense part.
-    1) The dense part is an nn.Linear module that is replicated across all trainers using DistributedDataParallel.
-    2) The sparse part is a Remote Module that holds an nn.EmbeddingBag on the parameter server.
-    This remote model can get a Remote Reference to the embedding table on the parameter server.
+    하이브리드 모델은 희소 부분과 밀집 부분이 있는 모델입니다.
+    1) 밀집 부분은 분산 데이터 병렬을 사용하여 모든 트레이너에 걸쳐 복제되는 nn.Linear 모듈입니다.
+    2) 희소 부분은 매개변수 서버에서 nn.EmbeddingBag를 가지고 있는 원격 모듈입니다.
+    이 원격 모델은 매개변수 서버의 임베딩 테이블에 대한 원격 참조(Remote Reference)를 얻을 수 있습니다.
     """
 
     def __init__(self, remote_emb_module, device):
@@ -38,16 +38,16 @@ class HybridModel(torch.nn.Module):
 # BEGIN setup_trainer
 def _run_trainer(remote_emb_module, rank):
     r"""
-    각 트레이너는 매개변수 서버(parameter server)에서 임베딩 룩업(embedding lookup)을 포함하고
-    전역에서 nn.Linear를 실행하는 전방 전달(forward pass)를 실행합니다.
-    후방 전달(backward pass) 동안에 DDP는 조밀한 부분(nn.Linear)에 대한 그래디언트(gradient) 집계를 담당하고
-    분산 autograd는 그래디언트 업데이트가 매개변수 서버로 전파되도록 합니다.
+    각 트레이너는 매개변수 서버에서 임베딩 찾기 작업(embedding lookup)을 포함하고
+    전역에서 nn.Linear를 실행하는 순방향 전달(forward pass)를 실행합니다.
+    역방향 전달(backward pass) 동안에, 분산 데이터 병렬은 밀집 부분(nn.Linear)에 대한 변화도 집계를 담당하고
+    분산 autograd는 변화도 업데이트가 매개변수 서버로 전파되도록 합니다.
     """
 
     # 모델을 설정합니다.
     model = HybridModel(remote_emb_module, rank)
 
-    # 모든 모델 매개변수를 DistributedOptimizer에 대한 rref로 검색합니다.
+    # 모든 모델 매개변수를 DistributedOptimizer에 대한 rrefs로 검색합니다.
 
     # 임베딩 테이블에 대한 매개변수를 검색합니다.
     model_parameter_rrefs = model.remote_emb_module.remote_parameters()
@@ -59,7 +59,7 @@ def _run_trainer(remote_emb_module, rank):
     for param in model.fc.parameters():
         model_parameter_rrefs.append(RRef(param))
 
-    # Distributed optimizer을 설정합니다.
+    # 분산 옵티마이저를 설정합니다.
     opt = DistributedOptimizer(
         optim.SGD,
         model_parameter_rrefs,
@@ -88,24 +88,22 @@ def _run_trainer(remote_emb_module, rank):
             target = torch.LongTensor(batch_size).random_(8).cuda(rank)
             yield indices, offsets_tensor, target
 
-    # 100개의 에폭크(ephoc)에 대해 학습합니다.
+    # 100개의 에폭(epoch)에 대해 학습합니다.
     for epoch in range(100):
-        # Distributed autograd context를 생성합니다.
+        # 분산 autograd context를 생성합니다.
         for indices, offsets, target in get_next_batch(rank):
             with dist_autograd.context() as context_id:
                 output = model(indices, offsets)
                 loss = criterion(output, target)
 
-                # 분산 후방 전달(distributed backward pass)을 실행합니다.
+                # 분산 역방향 전달(distributed backward pass)을 실행합니다.
                 dist_autograd.backward(context_id, [loss])
 
-                # Distributed optimizer를 실행시킵니다.
+                # 분산 최적화를 갱신합니다.
                 opt.step(context_id)
 
-                # Not necessary to zero grads as each iteration creates a different
-                # distributed autograd context which hosts different grads
-                # 반복할 때마다 다른 그래디언트를 호스팅하는 하나의 다른 distributed autograd context를 생성하므로
-                # 그래디언트를 0으로 만들 필요가 없습니다.
+                # 반복될 때마다 다른 변화도를 호스팅하는 하나의 다른 분산 autograd context를 생성하므로
+                # 변화도를 0으로 만들 필요가 없습니다.
         print("Training done for epoch {}".format(epoch))
         # END run_trainer
 
@@ -121,7 +119,7 @@ def run_worker(rank, world_size):
     rpc_backend_options = TensorPipeRpcBackendOptions()
     rpc_backend_options.init_method = "tcp://localhost:29501"
 
-    # Rank 2 는  master를 의미하고, 3은 is 매개변수 서버, 그리고 0과 1은 트레이너를 뜻한다.
+    # Rank 2는  master를 의미하고, 3은 is 매개변수 서버, 그리고 0과 1은 트레이너를 뜻합니다.
     if rank == 2:
         rpc.init_rpc(
             "master",
@@ -150,7 +148,7 @@ def run_worker(rank, world_size):
         for fut in futs:
             fut.wait()
     elif rank <= 1:
-        # 트레이너에서 분산 데이터 병렬(Distributed DataParallel)에 대한 프로세스 그룹을 초기화합니다.
+        # 트레이너에서 분산 데이터 병렬에 대한 프로세스 그룹을 초기화합니다.
         dist.init_process_group(
             backend="gloo", rank=rank, world_size=2, init_method="tcp://localhost:29500"
         )
