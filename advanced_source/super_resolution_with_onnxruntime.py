ac5a82b90ca26c6202216b7003e51e9719eb8bc4
WooWonjin <dndnjswls613@naver.com>
2022-02-12 21:32:19 +0900

    super_resolution_with_onnxruntime.py 주석 오타 수정 (#431)
    super_resolution_with_onnxruntime.py 주석 오타 수정

--- advanced_source/super_resolution_with_onnxruntime.py
+++ advanced_source/super_resolution_with_onnxruntime.py
@@ -126,7 +126,7 @@ torch.onnx.export(torch_model,               # 실행될 모델
                   "super_resolution.onnx",   # 모델 저장 경로 (파일 또는 파일과 유사한 객체 모두 가능)
                   export_params=True,        # 모델 파일 안에 학습된 모델 가중치를 저장할지의 여부
                   opset_version=10,          # 모델을 변환할 때 사용할 ONNX 버전
-                  do_constant_folding=True,  # 최적하시 상수폴딩을 사용할지의 여부
+                  do_constant_folding=True,  # 최적화시 상수폴딩을 사용할지의 여부
                   input_names = ['input'],   # 모델의 입력값을 가리키는 이름
                   output_names = ['output'], # 모델의 출력값을 가리키는 이름
                   dynamic_axes={'input' : {0 : 'batch_size'},    # 가변적인 길이를 가진 차원
