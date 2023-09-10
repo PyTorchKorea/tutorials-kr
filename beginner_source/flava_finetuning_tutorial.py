c7a015b1e4fed713b53e4c7cba5b2b83b6ecc3de
chanmuzi <chanmuzi@naver.com>
2023-09-10 21:46:47 +0900

    beginner_source/flava_finetuning_tutorial.py 문법 및 렌더링 오류 수정

--- beginner_source/flava_finetuning_tutorial.py
+++ beginner_source/flava_finetuning_tutorial.py
@@ -2,16 +2,20 @@
 """
 TorchMultimodal 튜토리얼: FLAVA 미세조정
 ============================================
+
+**번역:** `김찬 <https://github.com/chanmuzi>`__
+
 """
-# **번역:** `김찬 <https://github.com/chanmzui>`_
+
+
 ######################################################################
 # 멀티 모달 AI는 최근에 이미지 캡셔닝, 시각적 검색부터 텍스트로부터 이미지를 생성같은
 # 최근의 응용까지 그 사용이 빠르게 확산되고 있습니다. **TorchMultimodal은 PyTorch를
-# 기반으로 하는 라이브러리로, 다중 모달 연구를 가능하게 하고 가속화하기 위한 빌딩 블록과
+# 기반으로 하는 라이브러리로, 멀티 모달 연구를 가능하게 하고 가속화하기 위한 빌딩 블록과
 # end-to-end 예제들을 제공합니다**.
 #
-# 본 튜토리얼에서는 **사전 훈련된 SoTA 모델인** `FLAVA <https://arxiv.org/pdf/2112.04482.pdf>`__ **를 
-# TorchMultimodal 라이브러리에서 사용하여 멀티 모달 작업인 시각적 질의 응답**(VQA)에 미세조정하는 방법을 보여 드리겠습니다.
+# 본 튜토리얼에서는 **사전 훈련된 SoTA 모델인** `FLAVA <https://arxiv.org/pdf/2112.04482.pdf>`__ **를**
+# **TorchMultimodal 라이브러리에서 사용하여 멀티 모달 작업인 시각적 질의 응답(VQA)에 미세조정하는 방법을 보여 드리겠습니다.**
 # 이 모델은 텍스트와 이미지를 위한 두 개의 단일 모달 트랜스포머 기반 인코더와
 # 두 임베딩을 결합하는 다중 모달 인코더로 구성되어 있습니다.
 # 이 모델은 대조적, 이미지-텍스트 매칭, 그리고 텍스트, 이미지 및 다중 모달 마스킹 손실을 사용하여 사전 훈련되었습니다.
@@ -21,7 +25,7 @@ TorchMultimodal 튜토리얼: FLAVA 미세조정
 ######################################################################
 # 설치
 # -----------------
-# 이 튜토리얼을 위해서는 TextVQA 데이터셋과 Hugging Face의 ``bert 토크나이저``를 사용할 것입니다.
+# 이 튜토리얼을 위해서는 TextVQA 데이터셋과 Hugging Face의 ``bert 토크나이저`` 를 사용할 것입니다.
 # 따라서 TorchMultimodal 외에도 datasets과 transformers를 설치해야 합니다.
 #
 # .. note::
@@ -54,7 +58,7 @@ TorchMultimodal 튜토리얼: FLAVA 미세조정
 # 2. 본 튜토리얼에서는 VQA를 이미지와 질문(텍스트)이 입력되고 출력이 답변 클래스인 분류 작업으로 취급합니다.
 #    따라서 답변 클래스와 레이블 매핑을 생성할 단어장 파일을 다운로드해야 합니다.
 #
-#    또한 Hugging Face에서 `textvqa 데이터셋 <https://arxiv.org/pdf/1904.08920.pdf>`__을 불러오는데, 
+#    또한 Hugging Face에서 `textvqa 데이터셋 <https://arxiv.org/pdf/1904.08920.pdf>`__ 을 불러오는데, 
 #    이 데이터셋은 34602개의 훈련 샘플(이미지, 질문, 답변)을 포함하고 있습니다.
 #
 # 3997개의 답변 클래스가 있음을 확인할 수 있으며, 이에는 알 수 없는 답변을 나타내는 클래스도 포함되어 있습니다.
@@ -87,9 +91,9 @@ plt.show()
 
 
 ######################################################################
-# 3. 다음으로, 이미지와 텍스트를 모델에서 사용할 수 있는 텐서로 변환하기 위한 변환 함수를 작성합니다.
-# - 이미지의 경우, torchvision의 변환을 사용하여 텐서로 변환하고 일정한 크기로 조정합니다.
-# - 텍스트의 경우, Hugging Face의 ``BertTokenizer``를 사용하여 토큰화(및 패딩)합니다.
+# 3. 다음으로, 이미지와 텍스트를 모델에서 사용할 수 있는 텐서로 변환하기 위한 변환 함수를 작성합니다. 
+# - 이미지의 경우, torchvision의 변환을 사용하여 텐서로 변환하고 일정한 크기로 조정합니다. 
+# - 텍스트의 경우, Hugging Face의 ``BertTokenizer`` 를 사용하여 토큰화(및 패딩)합니다. 
 # - 답변(즉, 레이블)의 경우, 가장 빈번하게 나타나는 답변을 훈련 레이블로 사용합니다:
 #
 
@@ -123,7 +127,7 @@ dataset.set_transform(transform)
 
 
 ######################################################################
-# 4. 마지막으로, ``torchmultimodal``에서 ``flava_model_for_classification``을 가져옵니다.
+# 4. 마지막으로, ``torchmultimodal`` 에서 ``flava_model_for_classification`` 을 가져옵니다.
 # 이것은 기본적으로 사전 훈련된 FLAVA 체크포인트를 로드하고 분류 헤드를 포함합니다.
 #
 # 모델의 순방향 함수는 이미지를 시각 인코더에 통과시키고 질문을 텍스트 인코더에 통과시킵니다.
@@ -167,7 +171,7 @@ for _ in range(epochs):
 # -------------------
 #
 # 이 튜토리얼에서는 TorchMultimodal의 FLAVA를 사용하여 멀티 모달 작업에 미세 조정하는
-# 기본적인 방식을 소개했습니다. 객체 탐지를 위한 멀티 모달 모델인 `MDETR <https://github.com/facebookresearch/multimodal/tree/main/torchmultimodal/models/mdetr>`__과
+# 기본적인 방식을 소개했습니다. 객체 탐지를 위한 멀티 모달 모델인 `MDETR <https://github.com/facebookresearch/multimodal/tree/main/torchmultimodal/models/mdetr>`__ 과
 # 이미지, 비디오, 3D 분류를 포괄하는 다작업 모델 `Omnivore <https://github.com/facebookresearch/multimodal/blob/main/torchmultimodal/models/omnivore.py>`__
 # 같은 라이브러리의 다른 예제들도 확인해 보세요.
 # 
